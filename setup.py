1a83f6799e88fbfd3e037262cd7704bfa042a9c6
pseudo-rnd-thoughts <mark.m.towers@gmail.com>
2022-09-13 10:24:54 +0100

    Update to gymnasium notices

--- setup.py
+++ setup.py
@@ -65,7 +65,7 @@ setup(
         "numpy >= 1.18.0",
         "cloudpickle >= 1.2.0",
         "importlib_metadata >= 4.8.0; python_version < '3.10'",
-        "gym_notices >= 0.0.4",
+        "gymnasium_notices >= 0.0.4",
         "dataclasses == 0.8; python_version == '3.6'",
     ],
     license="MIT",
