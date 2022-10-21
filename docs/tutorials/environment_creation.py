1a0bc9b710aadb08186f2da8e8757abbbf6eda64
Manuel Goulão <msilvagoulao@gmail.com>
2022-10-21 12:00:32 +0100

    update tutorials

--- docs/tutorials/environment_creation.py
+++ docs/tutorials/environment_creation.py
@@ -5,8 +5,7 @@ Make your own custom environment
 This documentation overviews creating new environments and relevant
 useful wrappers, utilities and tests included in Gymnasium designed for
 the creation of new environments. You can clone gym-examples to play
-with the code that is presented here. We recommend that you use a
-virtual environment:
+with the code that is presented here. We recommend that you use a virtual environment:
 
 .. code:: console
 
@@ -153,9 +152,9 @@ class GridWorldEnv(gym.Env):
 # this is not mandatory and you may as well compute observations in
 # ``reset`` and ``step`` separately:
 
-
-def _get_obs(self):
-    return {"agent": self._agent_location, "target": self._target_location}
+class GridWorldEnv(gym.Env):
+    def _get_obs(self):
+        return {"agent": self._agent_location, "target": self._target_location}
 
 
 # %%
@@ -163,11 +162,11 @@ def _get_obs(self):
 # that is returned by ``step`` and ``reset``. In our case, we would like
 # to provide the manhattan distance between the agent and the target:
 
-
-def _get_info(self):
-    return {
-        "distance": np.linalg.norm(self._agent_location - self._target_location, ord=1)
-    }
+class GridWorldEnv(gym.Env):
+    def _get_info(self):
+        return {
+            "distance": np.linalg.norm(self._agent_location - self._target_location, ord=1)
+        }
 
 
 # %%
@@ -199,26 +198,26 @@ def _get_info(self):
 # and some auxiliary information. We can use the methods ``_get_obs`` and
 # ``_get_info`` that we implemented earlier for that:
 
+class GridWorldEnv(gym.Env):
+    def reset(self, seed=None, options=None):
+        # We need the following line to seed self.np_random
+        super().reset(seed=seed)
 
-def reset(self, seed=None, options=None):
-    # We need the following line to seed self.np_random
-    super().reset(seed=seed)
+        # Choose the agent's location uniformly at random
+        self._agent_location = self.np_random.integers(0, self.size, size=2, dtype=int)
 
-    # Choose the agent's location uniformly at random
-    self._agent_location = self.np_random.integers(0, self.size, size=2, dtype=int)
+        # We will sample the target's location randomly until it does not coincide with the agent's location
+        self._target_location = self._agent_location
+        while np.array_equal(self._target_location, self._agent_location):
+            self._target_location = self.np_random.integers(0, self.size, size=2, dtype=int)
 
-    # We will sample the target's location randomly until it does not coincide with the agent's location
-    self._target_location = self._agent_location
-    while np.array_equal(self._target_location, self._agent_location):
-        self._target_location = self.np_random.integers(0, self.size, size=2, dtype=int)
+        observation = self._get_obs()
+        info = self._get_info()
 
-    observation = self._get_obs()
-    info = self._get_info()
+        if self.render_mode == "human":
+            self._render_frame()
 
-    if self.render_mode == "human":
-        self._render_frame()
-
-    return observation, info
+        return observation, info
 
 
 # %%
@@ -235,22 +234,22 @@ def reset(self, seed=None, options=None):
 # know ``done``. To gather ``observation`` and ``info``, we can again make
 # use of ``_get_obs`` and ``_get_info``:
 
+class GridWorldEnv(gym.Env):
+    def step(self, action):
+        # Map the action (element of {0,1,2,3}) to the direction we walk in
+        direction = self._action_to_direction[action]
+        # We use `np.clip` to make sure we don't leave the grid
+        self._agent_location = np.clip(self._agent_location + direction, 0, self.size - 1)
+        # An episode is done iff the agent has reached the target
+        terminated = np.array_equal(self._agent_location, self._target_location)
+        reward = 1 if terminated else 0  # Binary sparse rewards
+        observation = self._get_obs()
+        info = self._get_info()
 
-def step(self, action):
-    # Map the action (element of {0,1,2,3}) to the direction we walk in
-    direction = self._action_to_direction[action]
-    # We use `np.clip` to make sure we don't leave the grid
-    self._agent_location = np.clip(self._agent_location + direction, 0, self.size - 1)
-    # An episode is done iff the agent has reached the target
-    terminated = np.array_equal(self._agent_location, self._target_location)
-    reward = 1 if terminated else 0  # Binary sparse rewards
-    observation = self._get_obs()
-    info = self._get_info()
+        if self.render_mode == "human":
+            self._render_frame()
 
-    if self.render_mode == "human":
-        self._render_frame()
-
-    return observation, reward, terminated, False, info
+        return observation, reward, terminated, False, info
 
 
 # %%
@@ -261,72 +260,72 @@ def step(self, action):
 # is used in many environments that are included with Gymnasium and you
 # can use it as a skeleton for your own environments:
 
+class GridWorldEnv(gym.Env):
+    def render(self):
+        if self.render_mode == "rgb_array":
+            return self._render_frame()
 
-def render(self):
-    if self.render_mode == "rgb_array":
-        return self._render_frame()
+        def _render_frame(self):
+            if self.window is None and self.render_mode == "human":
+                pygame.init()
+                pygame.display.init()
+                self.window = pygame.display.set_mode((self.window_size, self.window_size))
+            if self.clock is None and self.render_mode == "human":
+                self.clock = pygame.time.Clock()
 
-    def _render_frame(self):
-        if self.window is None and self.render_mode == "human":
-            pygame.init()
-            pygame.display.init()
-            self.window = pygame.display.set_mode((self.window_size, self.window_size))
-        if self.clock is None and self.render_mode == "human":
-            self.clock = pygame.time.Clock()
+            canvas = pygame.Surface((self.window_size, self.window_size))
+            canvas.fill((255, 255, 255))
+            pix_square_size = (
+                self.window_size / self.size
+            )  # The size of a single grid square in pixels
 
-        canvas = pygame.Surface((self.window_size, self.window_size))
-        canvas.fill((255, 255, 255))
-        pix_square_size = (
-            self.window_size / self.size
-        )  # The size of a single grid square in pixels
-
-        # First we draw the target
-        pygame.draw.rect(
-            canvas,
-            (255, 0, 0),
-            pygame.Rect(
-                pix_square_size * self._target_location,
-                (pix_square_size, pix_square_size),
-            ),
-        )
-        # Now we draw the agent
-        pygame.draw.circle(
-            canvas,
-            (0, 0, 255),
-            (self._agent_location + 0.5) * pix_square_size,
-            pix_square_size / 3,
-        )
-
-        # Finally, add some gridlines
-        for x in range(self.size + 1):
-            pygame.draw.line(
+            # First we draw the target
+            pygame.draw.rect(
                 canvas,
-                0,
-                (0, pix_square_size * x),
-                (self.window_size, pix_square_size * x),
-                width=3,
+                (255, 0, 0),
+                pygame.Rect(
+                    pix_square_size * self._target_location,
+                    (pix_square_size, pix_square_size),
+                ),
             )
-            pygame.draw.line(
+            # Now we draw the agent
+            pygame.draw.circle(
                 canvas,
-                0,
-                (pix_square_size * x, 0),
-                (pix_square_size * x, self.window_size),
-                width=3,
+                (0, 0, 255),
+                (self._agent_location + 0.5) * pix_square_size,
+                pix_square_size / 3,
             )
 
-        if self.render_mode == "human":
-            # The following line copies our drawings from `canvas` to the visible window
-            self.window.blit(canvas, canvas.get_rect())
-            pygame.event.pump()
-            pygame.display.update()
+            # Finally, add some gridlines
+            for x in range(self.size + 1):
+                pygame.draw.line(
+                    canvas,
+                    0,
+                    (0, pix_square_size * x),
+                    (self.window_size, pix_square_size * x),
+                    width=3,
+                )
+                pygame.draw.line(
+                    canvas,
+                    0,
+                    (pix_square_size * x, 0),
+                    (pix_square_size * x, self.window_size),
+                    width=3,
+                )
 
-            # We need to ensure that human-rendering occurs at the predefined framerate.
-            # The following line will automatically add a delay to keep the framerate stable.
-            self.clock.tick(self.metadata["render_fps"])
-        else:  # rgb_array
-            return np.transpose(
-                np.array(pygame.surfarray.pixels3d(canvas)), axes=(1, 0, 2)
-            )
+            if self.render_mode == "human":
+                # The following line copies our drawings from `canvas` to the visible window
+                self.window.blit(canvas, canvas.get_rect())
+                pygame.event.pump()
+                pygame.display.update()
+
+                # We need to ensure that human-rendering occurs at the predefined framerate.
+                # The following line will automatically add a delay to keep the framerate stable.
+                self.clock.tick(self.metadata["render_fps"])
+            else:  # rgb_array
+                return np.transpose(
+                    np.array(pygame.surfarray.pixels3d(canvas)), axes=(1, 0, 2)
+                )
 
 
 # %%
@@ -338,11 +337,11 @@ def render(self):
 # implement this method. However, in our example ``render_mode`` may be
 # ``"human"`` and we might need to close the window that has been opened:
 
-
-def close(self):
-    if self.window is not None:
-        pygame.display.quit()
-        pygame.quit()
+class GridWorldEnv(gym.Env):
+    def close(self):
+        if self.window is not None:
+            pygame.display.quit()
+            pygame.quit()
 
 
 # %%
@@ -357,14 +356,16 @@ def close(self):
 # In order for the custom environments to be detected by Gymnasium, they
 # must be registered as follows. We will choose to put this code in
 # ``gym-examples/gym_examples/__init__.py``.
-
-from gymnasium.envs.registration import register
-
-register(
-    id="gym_examples/GridWorld-v0",
-    entry_point="gym_examples.envs:GridWorldEnv",
-    max_episode_steps=300,
-)
+#
+# .. code:: python
+#
+#   from gymnasium.envs.registration import register
+#
+#   register(
+#        id="gym_examples/GridWorld-v0",
+#        entry_point="gym_examples.envs:GridWorldEnv",
+#        max_episode_steps=300,
+#   )
 
 # %%
 # The environment ID consists of three components, two of which are
