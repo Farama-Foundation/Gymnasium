46b43bf88c555668e90e5f919be346cfac5898a9
Markus Krimmel <montcyril@gmail.com>
2022-11-07 21:35:45 +0100

    Added testing for render return

--- gymnasium/utils/passive_env_checker.py
+++ gymnasium/utils/passive_env_checker.py
@@ -265,6 +265,48 @@ def env_step_passive_checker(env, action):
     return result
 
 
+def _check_render_return(render_mode, render_return):
+    """Produces warning if `render_return` doesn't match `render_mode`."""
+    # This many if statements are ugly, but we need it for short-circuiting in the rgb_array case (dict approach become difficult here)
+    if render_mode == "human" and render_return is not None:
+        logger.warn(f"Human rendering should return `None`, got {type(render_return)}")
+    if render_mode == "rgb_array":
+        if not isinstance(render_return, np.ndarray):
+            logger.warn(
+                f"RGB-array rendering should return a numpy array, got {type(render_return)}"
+            )
+        if isinstance(render_return, np.ndarray) and render_return.dtype != np.uint8:
+            logger.warn(
+                f"RGB-array rendering should return a numpy array with dtype uint8, got {render_return.dtype}"
+            )
+        if isinstance(render_return, np.ndarray) and render_return.ndim != 3:
+            logger.warn(
+                f"RGB-array rendering should return a numpy array with three axes, got {render_return.ndim}"
+            )
+        if (
+            isinstance(render_return, np.ndarray)
+            and render_return.ndim == 3
+            and render_return.shape[2] != 3
+        ):
+            logger.warn(
+                f"RGB-array rendering should return a numpy array in which the last axis has three dimensions, got {render_return.shape[2]}"
+            )
+    if render_mode == "ansi" and not isinstance(render_return, str):
+        logger.warn(
+            f"ANSI rendering should produce a string, got {type(render_return)}"
+        )
+    if render_mode.endswith("_list"):
+        if not isinstance(render_return, list):
+            logger.warn(
+                f"Render mode `{render_mode}` should produce a list, got {type(render_return)}"
+            )
+        else:
+            base_render_mode = render_mode[: -len("_list")]
+            [
+                _check_render_return(base_render_mode, item) for item in render_return
+            ]  # Check that each item of the list matches the base render mode
+
+
 def env_render_passive_checker(env, *args, **kwargs):
     """A passive check of the `Env.render` that the declared render modes/fps in the metadata of the environment is declared."""
     render_modes = env.metadata.get("render_modes")
@@ -314,7 +356,7 @@ def env_render_passive_checker(env, *args, **kwargs):
             )
 
     result = env.render(*args, **kwargs)
-
-    # TODO: Check that the result is correct
+    if env.render_mode is not None:
+        _check_render_return(env.render_mode, result)
 
     return result
