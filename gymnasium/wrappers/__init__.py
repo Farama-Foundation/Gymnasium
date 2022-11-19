430f124cca3cc743e1fec941f845b38c8cd985be
Gianluca De Cola <42657588+gianlucadecola@users.noreply.github.com>
2022-11-20 00:57:10 +0100

    Basic lambda wrappers (#148)

--- gymnasium/wrappers/__init__.py
+++ gymnasium/wrappers/__init__.py
@@ -1,4 +1,8 @@
 """Module of wrapper classes."""
+from gymnasium import error
+from gymnasium.dev_wrappers.lambda_action import LambdaActionV0
+from gymnasium.dev_wrappers.lambda_observations import LambdaObservationsV0
+from gymnasium.dev_wrappers.lambda_reward import ClipRewardsV0, LambdaRewardV0
 from gymnasium.wrappers.atari_preprocessing import AtariPreprocessing
 from gymnasium.wrappers.autoreset import AutoResetWrapper
 from gymnasium.wrappers.clip_action import ClipAction
