a7fe95fefc83d04bb0215a1cd7d02e063ae8bf54
Kevlyn Kadamala <kevlyn@gmail.com>
2022-11-19 15:33:32 +0000

    Edit function params to accept cols instead of rows

--- tests/envs/test_pprint_registry.py
+++ tests/envs/test_pprint_registry.py
@@ -10,11 +10,10 @@ def test_pprint_custom_registry():
         "CartPole-v0": gym.envs.registry["CartPole-v0"],
         "CartPole-v1": gym.envs.registry["CartPole-v1"],
     }
-    out = gym.pprint_registry(a, disable_print=True)
+    out = gym.pprint_registry(a, disable_print=True, test=1)
 
     correct_out = """===== classic_control =====
-CartPole-v0 
-CartPole-v1 
+CartPole-v0 CartPole-v1 
 
 """
     assert out == correct_out
@@ -22,29 +21,19 @@ CartPole-v1
 
 def test_pprint_registry():
     """Testing the default registry, with no changes."""
-    out = gym.pprint_registry(disable_print=True)
+    out = gym.pprint_registry(disable_print=True, test=2)
 
     correct_out = """===== classic_control =====
-Acrobot-v1                   
-CartPole-v0                  
-CartPole-v1                  
-MountainCar-v0               
-MountainCarContinuous-v0     
-Pendulum-v1                  
+Acrobot-v1                   CartPole-v0                  CartPole-v1                  
+MountainCar-v0               MountainCarContinuous-v0     Pendulum-v1                  
 
 ===== box2d =====
-BipedalWalker-v3             
-BipedalWalkerHardcore-v3     
-CarRacing-v2                 
-LunarLander-v2               
-LunarLanderContinuous-v2     
+BipedalWalker-v3             BipedalWalkerHardcore-v3     CarRacing-v2                 
+LunarLander-v2               LunarLanderContinuous-v2     
 
 ===== toy_text =====
-Blackjack-v1                 
-CliffWalking-v0              
-FrozenLake-v1                
-FrozenLake8x8-v1             
-Taxi-v3                      
+Blackjack-v1                 CliffWalking-v0              FrozenLake-v1                
+FrozenLake8x8-v1             Taxi-v3                      
 
 ===== mujoco =====
 Ant-v2                       Ant-v3                       Ant-v4                       
@@ -62,14 +51,10 @@ Walker2d-v4
 GymV26Environment-v0         
 
 ===== utils_envs =====
-RegisterDuringMakeEnv-v0     
-test.ArgumentEnv-v0          
-test.OrderlessArgumentEnv-v0 
+RegisterDuringMakeEnv-v0     test.ArgumentEnv-v0          test.OrderlessArgumentEnv-v0 
 
 ===== test =====
-test/NoHuman-v0              
-test/NoHumanNoRGB-v0         
-test/NoHumanOldAPI-v0        
+test/NoHuman-v0              test/NoHumanNoRGB-v0         test/NoHumanOldAPI-v0        
 
 """
     assert out == correct_out
@@ -78,21 +63,17 @@ test/NoHumanOldAPI-v0
 def test_pprint_registry_exclude_namespaces():
     """Testing the default registry, with no changes."""
     out = gym.pprint_registry(
-        max_rows=20, exclude_namespaces=["classic_control"], disable_print=True
+        num_cols=2, exclude_namespaces=["classic_control"], disable_print=True, test=3
     )
 
     correct_out = """===== box2d =====
-BipedalWalker-v3             
-BipedalWalkerHardcore-v3     
-CarRacing-v2                 
-LunarLander-v2               
+BipedalWalker-v3             BipedalWalkerHardcore-v3     
+CarRacing-v2                 LunarLander-v2               
 LunarLanderContinuous-v2     
 
 ===== toy_text =====
-Blackjack-v1                 
-CliffWalking-v0              
-FrozenLake-v1                
-FrozenLake8x8-v1             
+Blackjack-v1                 CliffWalking-v0              
+FrozenLake-v1                FrozenLake8x8-v1             
 Taxi-v3                      
 
 ===== mujoco =====
@@ -115,13 +96,11 @@ Walker2d-v3                  Walker2d-v4
 GymV26Environment-v0         
 
 ===== utils_envs =====
-RegisterDuringMakeEnv-v0     
-test.ArgumentEnv-v0          
+RegisterDuringMakeEnv-v0     test.ArgumentEnv-v0          
 test.OrderlessArgumentEnv-v0 
 
 ===== test =====
-test/NoHuman-v0              
-test/NoHumanNoRGB-v0         
+test/NoHuman-v0              test/NoHumanNoRGB-v0         
 test/NoHumanOldAPI-v0        
 
 """
@@ -132,29 +111,19 @@ def test_pprint_registry_no_entry_point():
     """Test registry if there is environment with no entry point."""
 
     gym.register("NoNamespaceEnv", "no-entry-point")
-    out = gym.pprint_registry(disable_print=True)
+    out = gym.pprint_registry(disable_print=True, test=4)
 
     correct_out = """===== classic_control =====
-Acrobot-v1                   
-CartPole-v0                  
-CartPole-v1                  
-MountainCar-v0               
-MountainCarContinuous-v0     
-Pendulum-v1                  
+Acrobot-v1                   CartPole-v0                  CartPole-v1                  
+MountainCar-v0               MountainCarContinuous-v0     Pendulum-v1                  
 
 ===== box2d =====
-BipedalWalker-v3             
-BipedalWalkerHardcore-v3     
-CarRacing-v2                 
-LunarLander-v2               
-LunarLanderContinuous-v2     
+BipedalWalker-v3             BipedalWalkerHardcore-v3     CarRacing-v2                 
+LunarLander-v2               LunarLanderContinuous-v2     
 
 ===== toy_text =====
-Blackjack-v1                 
-CliffWalking-v0              
-FrozenLake-v1                
-FrozenLake8x8-v1             
-Taxi-v3                      
+Blackjack-v1                 CliffWalking-v0              FrozenLake-v1                
+FrozenLake8x8-v1             Taxi-v3                      
 
 ===== mujoco =====
 Ant-v2                       Ant-v3                       Ant-v4                       
@@ -172,14 +141,10 @@ Walker2d-v4
 GymV26Environment-v0         
 
 ===== utils_envs =====
-RegisterDuringMakeEnv-v0     
-test.ArgumentEnv-v0          
-test.OrderlessArgumentEnv-v0 
+RegisterDuringMakeEnv-v0     test.ArgumentEnv-v0          test.OrderlessArgumentEnv-v0 
 
 ===== test =====
-test/NoHuman-v0              
-test/NoHumanNoRGB-v0         
-test/NoHumanOldAPI-v0        
+test/NoHuman-v0              test/NoHumanNoRGB-v0         test/NoHumanOldAPI-v0        
 
 ===== NoNamespaceEnv =====
 NoNamespaceEnv               
