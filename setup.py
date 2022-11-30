0484aa1ac1e29e574a46a9e829c914c2b5e74c29
StringTheory <mark.m.towers@gmail.com>
2022-11-30 16:38:12 +0000

    Code review by Ariel

--- setup.py
+++ setup.py
@@ -38,7 +38,7 @@ extras = {
     "box2d": ["box2d-py==2.3.5", "pygame==2.1.0", "swig==4.*"],
     "classic_control": ["pygame==2.1.0"],
     "mujoco_py": ["mujoco_py<2.2,>=2.1"],
-    "mujoco": ["mujoco==2.3", "imageio>=2.14.1"],
+    "mujoco": ["mujoco==2.2", "imageio>=2.14.1"],
     "toy_text": ["pygame==2.1.0"],
     "jax": ["jax==0.3.20", "jaxlib==0.3.20"],
     "other": ["lz4>=3.1.0", "opencv-python>=3.0", "matplotlib>=3.0", "moviepy>=1.0.0"],
@@ -46,7 +46,6 @@ extras = {
 
 extras["testing"] = list(set(itertools.chain.from_iterable(extras.values()))) + [
     "pytest==7.1.3",
-    "gym==0.26.2",
 ]
 
 # All dependency groups - accept rom license as requires user to run
