2801a6025c59ce0ac6afbd148c88b0e68fc3ba63
Rodrigo de Lazcano Perez-Vicente <rlazcano@umd.edu>
2022-12-01 11:19:23 -0500

    revert camera id

--- gymnasium/envs/mujoco/mujoco_rendering.py
+++ gymnasium/envs/mujoco/mujoco_rendering.py
@@ -661,7 +661,11 @@ class MujocoRenderer:
                 camera_name = "track"
 
             if camera_id is None:
-                camera_id = self.model.camera(camera_name).id
+                camera_id = mujoco.mj_name2id(
+                    self.model,
+                    mujoco.mjtObj.mjOBJ_CAMERA,
+                    camera_name,
+                )
 
             img = viewer.render(render_mode=render_mode, camera_id=camera_id)
             return img
