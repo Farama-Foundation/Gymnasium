84c5c24e0858ad9941d2b0b725172b91a544218d
pseudo-rnd-thoughts <mark.m.towers@gmail.com>
2022-12-02 11:50:52 +0000

    Deploying to gh-pages from @ Farama-Foundation/Gymnasium@72fee2aa37f50bb3f1d9104ad05717133bcc485a 🚀

--- main/_static/documentation_options.js
+++ main/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.26.3',
+    VERSION: '0.27.0',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'dirhtml',
