0f34a9783b2cde8bcc8dea7437dc8f484e5574d5
younik <omar.younis98@gmail.com>
2022-12-06 22:01:41 +0100

    add deprecation warnings to wrappers

--- gymnasium/wrappers/__init__.py
+++ gymnasium/wrappers/__init__.py
@@ -46,27 +46,37 @@ If you need a wrapper to do more complicated tasks, you can inherit from the :cl
 
 If you'd like to implement your own custom wrapper, check out `the corresponding tutorial <../../tutorials/implementing_custom_wrappers>`_.
 """
-from gymnasium.wrappers.atari_preprocessing import AtariPreprocessing
-from gymnasium.wrappers.autoreset import AutoResetWrapper
-from gymnasium.wrappers.clip_action import ClipAction
-from gymnasium.wrappers.compatibility import EnvCompatibility
-from gymnasium.wrappers.env_checker import PassiveEnvChecker
-from gymnasium.wrappers.filter_observation import FilterObservation
-from gymnasium.wrappers.flatten_observation import FlattenObservation
-from gymnasium.wrappers.frame_stack import FrameStack, LazyFrames
-from gymnasium.wrappers.gray_scale_observation import GrayScaleObservation
-from gymnasium.wrappers.human_rendering import HumanRendering
-from gymnasium.wrappers.normalize import NormalizeObservation, NormalizeReward
-from gymnasium.wrappers.order_enforcing import OrderEnforcing
-from gymnasium.wrappers.pixel_observation import PixelObservationWrapper
-from gymnasium.wrappers.record_episode_statistics import RecordEpisodeStatistics
-from gymnasium.wrappers.record_video import RecordVideo, capped_cubic_video_schedule
-from gymnasium.wrappers.render_collection import RenderCollection
-from gymnasium.wrappers.rescale_action import RescaleAction
-from gymnasium.wrappers.resize_observation import ResizeObservation
-from gymnasium.wrappers.step_api_compatibility import StepAPICompatibility
-from gymnasium.wrappers.time_aware_observation import TimeAwareObservation
-from gymnasium.wrappers.time_limit import TimeLimit
-from gymnasium.wrappers.transform_observation import TransformObservation
-from gymnasium.wrappers.transform_reward import TransformReward
-from gymnasium.wrappers.vector_list_info import VectorListInfo
+from gymnasium.wrappers.atari_preprocessing import AtariPreprocessing as _AtariPreprocessing
+from gymnasium.wrappers.autoreset import AutoResetWrapper as _AutoResetWrapper
+from gymnasium.wrappers.clip_action import ClipAction as _ClipAction
+from gymnasium.wrappers.compatibility import EnvCompatibility as _EnvCompatibility
+from gymnasium.wrappers.env_checker import PassiveEnvChecker as _PassiveEnvChecker
+from gymnasium.wrappers.filter_observation import FilterObservation as _FilterObservation
+from gymnasium.wrappers.flatten_observation import FlattenObservation as _FlattenObservation
+from gymnasium.wrappers.frame_stack import FrameStack as _FrameStack
+from gymnasium.wrappers.frame_stack import LazyFrames as _LazyFrames
+from gymnasium.wrappers.gray_scale_observation import GrayScaleObservation as _GrayScaleObservation
+from gymnasium.wrappers.human_rendering import HumanRendering as _HumanRendering
+from gymnasium.wrappers.normalize import NormalizeObservation as _NormalizeObservation
+from gymnasium.wrappers.normalize import NormalizeReward as _NormalizeReward
+from gymnasium.wrappers.order_enforcing import OrderEnforcing as _OrderEnforcing
+from gymnasium.wrappers.pixel_observation import PixelObservationWrapper as _PixelObservationWrapper
+from gymnasium.wrappers.record_episode_statistics import RecordEpisodeStatistics as _RecordEpisodeStatistics
+from gymnasium.wrappers.record_video import RecordVideo as _RecordVideo
+from gymnasium.wrappers.record_video import capped_cubic_video_schedule as _capped_cubic_video_schedule
+from gymnasium.wrappers.render_collection import RenderCollection as _RenderCollection
+from gymnasium.wrappers.rescale_action import RescaleAction as _RescaleAction
+from gymnasium.wrappers.resize_observation import ResizeObservation as _ResizeObservation
+from gymnasium.wrappers.step_api_compatibility import StepAPICompatibility as _StepAPICompatibility
+from gymnasium.wrappers.time_aware_observation import TimeAwareObservation as _TimeAwareObservation
+from gymnasium.wrappers.time_limit import TimeLimit as _TimeLimit
+from gymnasium.wrappers.transform_observation import TransformObservation as _TransformObservation
+from gymnasium.wrappers.transform_reward import TransformReward as _TransformReward
+from gymnasium.wrappers.vector_list_info import VectorListInfo as _VectorListInfo
+from gymnasium import logger
+
+
+def __getattr__(name):
+    logger.warn("Wrappers have been replaced in favor of lambda wrappers and they will be deleted soon. "
+                "Use the ones in gymnasium.experimental.wrappers, for further details see: ")  # TODO
+    return globals()[f"_{name}"]
