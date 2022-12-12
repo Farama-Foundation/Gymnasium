82a1e08df4572d029985d9f41d594a3ea4ead69a
Jet <38184875+jjshoots@users.noreply.github.com>
2022-12-12 18:29:41 +0000

    Change env checker assertation to warning (#215)

--- gymnasium/utils/env_checker.py
+++ gymnasium/utils/env_checker.py
@@ -307,9 +307,13 @@ def check_env(env: gym.Env, warn: bool = None, skip_render_check: bool = False):
         if env.render_mode is not None:
             env_render_passive_checker(env)
 
-        for render_mode in env.metadata["render_modes"]:
-            assert env.spec is not None
-            new_env = env.spec.make(render_mode=render_mode)
-            new_env.reset()
-            env_render_passive_checker(new_env)
-            new_env.close()
+        if env.spec is not None:
+            for render_mode in env.metadata["render_modes"]:
+                new_env = env.spec.make(render_mode=render_mode)
+                new_env.reset()
+                env_render_passive_checker(new_env)
+                new_env.close()
+        else:
+            logger.warn(
+                "Not able to test alternative render modes due to the environment not having a spec. Try instantialising the environment through gymnasium.make"
+            )
