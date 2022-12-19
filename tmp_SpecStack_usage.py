45ced4c0a7d3e06beec20072433aab768eb18a49
will <will2346@live.co.uk>
2022-12-19 16:10:30 +0000

    clean up example

--- tmp_SpecStack_usage.py
+++ tmp_SpecStack_usage.py
@@ -18,18 +18,3 @@ print(as_json)
 reconstructed = deserialise_spec_stack(as_json, eval_ok=True)
 print(reconstructed)
 pprint_stack(as_json)
-
-# # Printing the spec stack
-# env_spec_stack = env.spec_stack
-# print(env_spec_stack)
-#
-# # Reconstructing the environment from the spec stack
-# reconstructed_env = gym.make(env_spec_stack)
-#
-# # spec stack as JSON
-# json_representation = reconstructed_env.spec_stack.json
-# reconstructed_env_from_json = gym.make(SpecStack(json_representation))      # spec_stack can be used to construct the environment
-#
-# # The two spec stacks should be identical
-# print(reconstructed_env_from_json.spec_stack)
-# print(reconstructed_env_from_json.spec_stack == env_spec_stack)
