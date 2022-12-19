dc28540ff06b422fa7f37964af1c7b1a5482d0ed
will <will2346@live.co.uk>
2022-12-19 17:37:04 +0000

    fix gym.make()

--- tmp_SpecStack_usage.py
+++ tmp_SpecStack_usage.py
@@ -22,4 +22,5 @@ stack_r = deserialise_spec_stack(as_json_r, eval_ok=True)  # deserialise from JS
 env_r = gym.make(stack_r)                                  # reconstruct the environment
 
 # visualise the spec stack of the reconstructed environment
-pprint_stack(serialise_spec_stack(env_r.spec_stack))
+pprint_stack(as_json_r)
+pprint_stack(serialise_spec_stack(env_r.spec_stack))  # NB: This fails, reserialisation of callable is not supported
