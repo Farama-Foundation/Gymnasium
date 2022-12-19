d74218c1b30934dfdc3c42fc922b7cf5340ee0e3
will <will2346@live.co.uk>
2022-12-19 17:27:09 +0000

    update example to standard pipeline

--- tmp_SpecStack_usage.py
+++ tmp_SpecStack_usage.py
@@ -11,10 +11,15 @@ env = gym.wrappers.TransformReward(env, lambda r: 0.01 * r)
 env = gym.wrappers.ResizeObservation(env, (84, 84))
 env = gym.wrappers.TransformObservation(env, lambda o: o / 255.0)
 
+# encoding process
+stack = env.spec_stack                                     # spec stack
+as_json = serialise_spec_stack(stack)                      # serialise to JSON
+as_string = json.dumps(as_json)                            # serialise to string
 
-print(env.spec_stack)
-as_json = serialise_spec_stack(env.spec_stack)
-print(as_json)
-reconstructed = deserialise_spec_stack(as_json, eval_ok=True)
-print(reconstructed)
-pprint_stack(as_json)
+# decoding process
+as_json_r = json.loads(as_string)                          # deserialise from string
+stack_r = deserialise_spec_stack(as_json_r, eval_ok=True)  # deserialise from JSON
+env_r = gym.make(stack_r)                                  # reconstruct the environment
+
+# visualise the spec stack of the reconstructed environment
+pprint_stack(serialise_spec_stack(env_r.spec_stack))
