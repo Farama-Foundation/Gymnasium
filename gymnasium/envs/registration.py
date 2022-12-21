13335b3fb4eedb3ddd0b53c57052e816941ded5e
StringTheory <mark.m.towers@gmail.com>
2022-12-21 11:56:54 +0000

    initial commit

--- gymnasium/envs/registration.py
+++ gymnasium/envs/registration.py
@@ -310,7 +310,7 @@ def load_env_plugins(entry_point: str = "gymnasium.envs") -> None:
             try:
                 fn()
             except Exception as e:
-                logger.warn(str(e))
+                logger.warn(f'plugin: {plugin.value} raised {e}')
 
 
 # fmt: off
