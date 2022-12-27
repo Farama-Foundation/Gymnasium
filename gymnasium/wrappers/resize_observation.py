ce7381fdb61fe865d81cee2cf949502c4f4997ef
will <will2346@live.co.uk>
2022-12-27 23:52:09 +0000

    added back ezpickle_args (rollback bug)

--- gymnasium/wrappers/resize_observation.py
+++ gymnasium/wrappers/resize_observation.py
@@ -45,7 +45,7 @@ class ResizeObservation(gym.ObservationWrapper, gym.utils.EzPickle):
         obs_shape = self.shape + env.observation_space.shape[2:]
         self.observation_space = Box(low=0, high=255, shape=obs_shape, dtype=np.uint8)
 
-        gym.utils.EzPickle.__init__(self, 6, shape=shape)
+        gym.utils.EzPickle.__init__(self, shape=shape)
 
     def observation(self, observation):
         """Updates the observations by resizing the observation to shape given by :attr:`shape`.
