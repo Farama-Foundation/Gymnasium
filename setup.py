b6ef26b9745de6777b527fc2f7e2ffef809e1844
Jet <38184875+jjshoots@users.noreply.github.com>
2023-01-06 22:11:11 +0000

    Add back long description to setup (#243)

--- setup.py
+++ setup.py
@@ -19,4 +19,19 @@ def get_version():
     raise RuntimeError("bad version data in __init__.py")
 
 
-setup(name="gymnasium", version=get_version())
+def get_description():
+    """Gets the description from the readme."""
+    with open("README.md") as fh:
+        long_description = ""
+        header_count = 0
+        for line in fh:
+            if line.startswith("##"):
+                header_count += 1
+            if header_count < 2:
+                long_description += line
+            else:
+                break
+    return long_description
+
+
+setup(name="gymnasium", version=get_version(), long_description=get_description())
