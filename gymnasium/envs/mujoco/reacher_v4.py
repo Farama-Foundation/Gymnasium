72d9f54c206ac402d1df5b3752ba007bc56dbea2
Mark Towers <mark.m.towers@gmail.com>
2023-01-09 13:12:07 +0000

    Revert "ENH: allow metadata["render_modes"] to be a set" (#251)

--- gymnasium/envs/mujoco/reacher_v4.py
+++ gymnasium/envs/mujoco/reacher_v4.py
@@ -122,11 +122,11 @@ class ReacherEnv(MujocoEnv, utils.EzPickle):
     """
 
     metadata = {
-        "render_modes": {
+        "render_modes": [
             "human",
             "rgb_array",
             "depth_array",
-        },
+        ],
         "render_fps": 50,
     }
 
