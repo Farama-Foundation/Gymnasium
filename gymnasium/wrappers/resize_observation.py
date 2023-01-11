6b7045d0a52811b5a89b48f581aa89d040762a83
Ariel Kwiatkowski <ariel.j.kwiatkowski@gmail.com>
2023-01-11 02:12:01 +0100

    Minor fixes for the resize observation wrapper (#254)

--- gymnasium/wrappers/resize_observation.py
+++ gymnasium/wrappers/resize_observation.py
@@ -49,10 +49,8 @@ class ResizeObservation(gym.ObservationWrapper):
         ), f"Expected the observation space to be Box, actual type: {type(env.observation_space)}"
         dims = len(env.observation_space.shape)
         assert (
-            2 <= dims <= 3
+            dims == 2 or dims == 3
         ), f"Expected the observation space to have 2 or 3 dimensions, got: {dims}"
-
-        self.shape = tuple(shape)
 
         obs_shape = self.shape + env.observation_space.shape[2:]
         self.observation_space = Box(low=0, high=255, shape=obs_shape, dtype=np.uint8)
