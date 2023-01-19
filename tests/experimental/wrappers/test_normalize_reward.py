ed35d7c09eb068e5b2921f102699b0fcbf8a6dfc
raphajaner <raphael.trumpp@tum.de>
2023-01-19 18:00:47 +0100

    correct step function

--- tests/experimental/wrappers/test_normalize_reward.py
+++ tests/experimental/wrappers/test_normalize_reward.py
@@ -1,11 +1,16 @@
 """Test suite for NormalizeRewardV0."""
+from gymnasium.core import ActType
 from gymnasium.experimental.wrappers import NormalizeRewardV0
 from tests.testing_env import GenericTestEnv
 
 
 def test_running_mean_normalize_reward_wrapper():
     """Tests that the property `_update_running_mean` freezes/continues the running statistics updating."""
-    env = GenericTestEnv()
+
+    def step_func(self, action: ActType):
+        return self.observation_space.sample(), 1.0, False, False, {}
+
+    env = GenericTestEnv(step_func=step_func)
     wrapped_env = NormalizeRewardV0(env)
 
     # Default value is True
