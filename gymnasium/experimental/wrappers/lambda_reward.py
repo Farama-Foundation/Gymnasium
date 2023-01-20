a99257072e7e3c209878ad6235cbde7d6311a9dd
raphajaner <raphael.trumpp@tum.de>
2023-01-20 11:36:47 +0100

    Adding a dim to the reward if not ve_env

--- gymnasium/experimental/wrappers/lambda_reward.py
+++ gymnasium/experimental/wrappers/lambda_reward.py
@@ -98,6 +98,10 @@ class NormalizeRewardV0(gym.Wrapper):
 
     The exponential moving average will have variance :math:`(1 - \gamma)^2`.
 
+    The property `_update_running_mean` allows to freeze/continue the running mean calculation of the reward
+    statistics. If `True` (default), the `RunningMeanStd` will get updated every time `self.normalize()` is called.
+    If False, the calculated statistics are used but not updated anymore; this may be used during evaluation.
+
     Note:
         The scaling depends on past trajectories and rewards will not be scaled correctly if the wrapper was newly
         instantiated or the policy was changed recently.
@@ -117,6 +121,7 @@ class NormalizeRewardV0(gym.Wrapper):
             gamma (float): The discount factor that is used in the exponential moving average.
         """
         super().__init__(env)
+        self.is_vector_env = getattr(env, "is_vector_env", False)
         self.rewards_running_means = RunningMeanStd(shape=())
         self.discounted_reward: float = 0.0
         self.gamma = gamma
@@ -127,10 +132,14 @@ class NormalizeRewardV0(gym.Wrapper):
     ) -> tuple[WrapperObsType, SupportsFloat, bool, bool, dict[str, Any]]:
         """Steps through the environment, normalizing the reward returned."""
         obs, reward, terminated, truncated, info = super().step(action)
-        self.discounted_reward = self.discounted_reward * self.gamma * (
-            1 - terminated
-        ) + float(reward)
-        return obs, self.normalize(float(reward)), terminated, truncated, info
+        if not self.is_vector_env:
+            reward = np.array([float(reward)])
+        self.discounted_reward = (
+            self.discounted_reward * self.gamma * (1 - terminated) + reward
+        )
+        if not self.is_vector_env:
+            reward = float(reward[0])
+        return obs, self.normalize(reward), terminated, truncated, info
 
     def normalize(self, reward):
         """Normalizes the rewards with the running mean rewards and their variance."""
--- tests/experimental/wrappers/test_normalize_reward.py
+++ tests/experimental/wrappers/test_normalize_reward.py
@@ -1,1 +1,38 @@
 """Test suite for NormalizeRewardV0."""
+import numpy as np
+
+from gymnasium.core import ActType
+from gymnasium.experimental.wrappers import NormalizeRewardV0
+from gymnasium.vector import SyncVectorEnv
+from tests.testing_env import GenericTestEnv
+
+
+def _make_reward_env():
+    """Functions that returns an `GenericTestEnv` with reward=1."""
+
+    def step_func(self, action: ActType):
+        return self.observation_space.sample(), 1.0, False, False, {}
+
+    return GenericTestEnv(step_func=step_func)
+
+
+def test_normalize_reward_wrapper():
+    """Tests that the NormalizeReward does not throw an error."""
+    # TODO: Functional correctness should be tested
+    env = _make_reward_env()
+    wrapped_env = NormalizeRewardV0(env)
+    wrapped_env.reset()
+    _, reward, _, _, _ = wrapped_env.step(None)
+    assert np.ndim(reward) == 0
+    env.close()
+
+
+def test_vec_normalize_reward_wrapper():
+    """Tests that the NormalizeReward does not throw an error for VectorEnv."""
+    # TODO: Functional correctness should be tested
+    envs = SyncVectorEnv([_make_reward_env, _make_reward_env])
+    wrapped_env = NormalizeRewardV0(envs)
+    wrapped_env.reset()
+    _, reward, _, _, _ = wrapped_env.step([None, None])
+    assert np.ndim(reward) > 0
+    envs.close
