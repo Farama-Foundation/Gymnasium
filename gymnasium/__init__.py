374300d370325ae0ac8e5dc282aad3d266e2df20
Michael G. Inso <68110223+MiChaelinzo@users.noreply.github.com>
2023-01-24 16:10:51 +0300

    Update __init__.py

--- gymnasium/__init__.py
+++ gymnasium/__init__.py
@@ -1,17 +1,27 @@
 """Root `__init__` of the gymnasium module setting the `__all__` of gymnasium modules."""
 # isort: skip_file
 
-from gymnasium.core import (
-    Env,
-    Wrapper,
-    ObservationWrapper,
-    ActionWrapper,
-    RewardWrapper,
-)
+import os
+import sys
+from gymnasium.core import (Env, Wrapper, ObservationWrapper, ActionWrapper, RewardWrapper)
 from gymnasium.spaces.space import Space
 from gymnasium.envs.registration import make, spec, register, registry, pprint_registry
 from gymnasium import envs, spaces, utils, vector, wrappers, error, logger, experimental
+import platform
 
+__version__ = "0.27.1"
+
+if platform.system() == "Linux":
+    os.environ["SDL_AUDIODRIVER"] = "dsp"
+
+try:
+    import gym_notices.notices as notices
+    os.environ["PYGAME_HIDE_SUPPORT_PROMPT"] = "hide"
+    notice = notices.notices.get(__version__)
+    if notice:
+        print(notice, file=sys.stderr)
+except Exception as e:
+    raise e
 
 __all__ = [
     # core classes
@@ -27,7 +37,7 @@ __all__ = [
     "register",
     "registry",
     "pprint_registry",
-    # module folders
+    # module Folders
     "envs",
     "spaces",
     "utils",
