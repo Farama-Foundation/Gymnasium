57f10f6ad63da15b70faa4c2c76dd092e82c1b0a
StringTheory <mark.m.towers@gmail.com>
2023-01-25 15:19:16 +0000

    Update __init__.py

--- gymnasium/utils/__init__.py
+++ gymnasium/utils/__init__.py
@@ -3,11 +3,10 @@
 These are not intended as API functions, and will not remain stable over time.
 """
 
-from gymnasium.utils import seeding
+from gymnasium.utils import play, seeding
 from gymnasium.utils.colorize import colorize
 from gymnasium.utils.env_checker import check_env
 from gymnasium.utils.ezpickle import EzPickle
-from gymnasium.utils.play import play
 from gymnasium.utils.save_video import capped_cubic_video_schedule, save_video
 from gymnasium.utils.step_api_compatibility import (
     convert_to_done_step_api,
@@ -17,11 +16,13 @@ from gymnasium.utils.step_api_compatibility import (
 
 
 __all__ = [
+    # Files
+    "seeding",
+    "play",  # due to the core function being called play as well, then we cannot shorten the link.
+    # Functions
     "colorize",
     "EzPickle",
-    "seeding",
     "check_env",
-    "play",
     "save_video",
     "capped_cubic_video_schedule",
     "step_api_compatibility",
