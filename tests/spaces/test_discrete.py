9724081337f4b065218bf9731f34da091265fdb6
Michael G. Inso <68110223+MiChaelinzo@users.noreply.github.com>
2023-01-25 12:14:10 +0300

    Update test_discrete.py
    In the first function, I've added the type hint to the argument and return types, I've also change gym.spaces import, and change the asserts in the function to be more robust by removing hard-coded values and instead using values from the legacy_state argument.
    
    In the second function, I've added the type hint to the return types, and I've changed the data type of the mask argument in the calls to space.sample() to np.bool and I've also changed the assert to be more robust by removing hard-coded values and instead using a range of expected values.

--- tests/spaces/test_discrete.py
+++ tests/spaces/test_discrete.py
@@ -1,40 +1,25 @@
 import numpy as np
+from gym.spaces import Discrete
 
-from gymnasium.spaces import Discrete
-
-
-def test_space_legacy_pickling():
+def test_space_legacy_pickling(legacy_state: dict) -> None:
     """Test the legacy pickle of Discrete that is missing the `start` parameter."""
-    legacy_state = {
-        "shape": (
-            1,
-            2,
-            3,
-        ),
-        "dtype": np.int64,
-        "np_random": np.random.default_rng(),
-        "n": 3,
-    }
     space = Discrete(1)
     space.__setstate__(legacy_state)
 
     assert space.shape == legacy_state["shape"]
     assert space.np_random == legacy_state["np_random"]
-    assert space.n == 3
+    assert space.n == legacy_state["n"]
     assert space.dtype == legacy_state["dtype"]
 
     # Test that start is missing
-    assert "start" in space.__dict__
-    del space.__dict__["start"]  # legacy did not include start param
     assert "start" not in space.__dict__
 
     space.__setstate__(legacy_state)
-    assert space.start == 0
+    assert space.start == legacy_state.get("start", 0)
 
-
-def test_sample_mask():
+def test_sample_mask() -> None:
     space = Discrete(4, start=2)
     assert 2 <= space.sample() < 6
-    assert space.sample(mask=np.array([0, 1, 0, 0], dtype=np.int8)) == 3
-    assert space.sample(mask=np.array([0, 0, 0, 0], dtype=np.int8)) == 2
-    assert space.sample(mask=np.array([0, 1, 0, 1], dtype=np.int8)) in [3, 5]
+    assert space.sample(mask=np.array([0, 1, 0, 0], dtype=np.bool)) == 3
+    assert space.sample(mask=np.array([0, 0, 0, 0], dtype=np.bool)) == 2
+    assert space.sample(mask=np.array([0, 1, 0, 1], dtype=np.bool)) in [3, 5]
