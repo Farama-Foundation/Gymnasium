f9351ef4a731e9d06113be47037071e4aaa79ecc
pseudo-rnd-thoughts <mark.m.towers@gmail.com>
2023-02-19 11:52:29 +0000

    Add test for custom space

--- tests/experimental/vector/utils/test_shared_memory.py
+++ tests/experimental/vector/utils/test_shared_memory.py
@@ -4,13 +4,14 @@ import multiprocessing as mp
 
 import pytest
 
+from gymnasium.error import CustomSpaceError
 from gymnasium.experimental.vector.utils import (
     create_shared_memory,
     read_from_shared_memory,
     write_to_shared_memory,
 )
 from gymnasium.utils.env_checker import data_equivalence
-from tests.spaces.utils import TESTING_SPACES, TESTING_SPACES_IDS
+from tests.spaces.utils import TESTING_SPACES, TESTING_SPACES_IDS, CUSTOM_SPACES, CUSTOM_SPACES_IDS
 
 
 @pytest.mark.parametrize("space", TESTING_SPACES, ids=TESTING_SPACES_IDS)
@@ -34,3 +35,9 @@ def test_shared_memory_create_read_write(space, num, ctx):
     read_samples = read_from_shared_memory(space, shared_memory, n=num)
     for read_sample, sample in zip(read_samples, samples):
         data_equivalence(read_sample, sample)
+
+
+@pytest.mark.parametrize("space", CUSTOM_SPACES, ids=CUSTOM_SPACES_IDS)
+def test_shared_memory_custom_space(space):
+    with pytest.raises(CustomSpaceError):
+        create_shared_memory(space)
