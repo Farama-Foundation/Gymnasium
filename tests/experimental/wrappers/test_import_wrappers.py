996d4f5b1671ade6638245f9b229005f1a4da1a5
Valentin Canete <valentin.canete@gmail.com>
2023-02-21 18:20:12 +0100

    add reglex escape to tests

--- tests/experimental/wrappers/test_import_wrappers.py
+++ tests/experimental/wrappers/test_import_wrappers.py
@@ -1,5 +1,7 @@
 """Test suite for import wrappers."""
 
+import re
+
 import pytest
 
 import gymnasium.experimental.wrappers as wrappers
@@ -9,17 +11,20 @@ def test_import_wrappers():
     """Test that all wrappers can be imported."""
     # Test that a deprecated wrapper raises a DeprecatedWrapper
     with pytest.raises(
-        wrappers.DeprecatedWrapper, match="NormalizeRewardV0 is now deprecated"
+        wrappers.DeprecatedWrapper,
+        match=re.escape("NormalizeRewardV0 is now deprecated"),
     ):
         getattr(wrappers, "NormalizeRewardV0")
 
     # Test that an invalid version raises an InvalidVersionWrapper
     with pytest.raises(
         wrappers.InvalidVersionWrapper,
-        match="ClipRewardVT is not a valid version number",
+        match=re.escape("ClipRewardVT is not a valid version number"),
     ):
         getattr(wrappers, "ClipRewardVT")
 
     # Test that an invalid wrapper raises an AttributeError
-    with pytest.raises(AttributeError, match="cannot import name 'NonexistentWrapper'"):
+    with pytest.raises(
+        AttributeError, match=re.escape("cannot import name 'NonexistentWrapper'")
+    ):
         getattr(wrappers, "NonexistentWrapper")
