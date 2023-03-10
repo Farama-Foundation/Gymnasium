6b0577eff196fd1a9aa952ea9f1f3dee7906cc11
Valentin Canete <valentin.canete@gmail.com>
2023-03-10 14:47:01 +0100

    fixed rendor_modes typo

--- tests/envs/test_compatibility.py
+++ tests/envs/test_compatibility.py
@@ -27,7 +27,7 @@ class LegacyEnvExplicit(LegacyEnv, gymnasium.Env):
 
     observation_space = Discrete(1)
     action_space = Discrete(1)
-    metadata = {"render.modes": ["human", "rgb_array"]}
+    metadata = {"render_modes": ["human", "rgb_array"]}
 
     def __init__(self):
         pass
@@ -56,7 +56,7 @@ class LegacyEnvImplicit(gymnasium.Env):
 
     observation_space = Discrete(1)
     action_space = Discrete(1)
-    metadata = {"render.modes": ["human", "rgb_array"]}
+    metadata = {"render_modes": ["human", "rgb_array"]}
 
     def __init__(self):
         pass
