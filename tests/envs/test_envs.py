2e1cd557bd92254a2e07e6a47e2adeb860323cfa
pseudo-rnd-thoughts <mark.m.towers@gmail.com>
2023-04-06 14:14:37 +0100

    Revert ignore warnings

--- tests/envs/test_envs.py
+++ tests/envs/test_envs.py
@@ -26,6 +26,9 @@ CHECK_ENV_IGNORE_WARNINGS = [
     f"\x1b[33mWARN: {message}\x1b[0m"
     for message in [
         "This version of the mujoco environments depends on the mujoco-py bindings, which are no longer maintained and may stop working. Please upgrade to the v4 versions of the environments (which depend on the mujoco python bindings instead), unless you are trying to precisely replicate previous works).",
+        "A Box observation space minimum value is -infinity. This is probably too low.",
+        "A Box observation space maximum value is -infinity. This is probably too high.",
+        "For Box action spaces, we recommend using a symmetric and normalized space (range=[-1, 1] or [0, 1]). See https://stable-baselines3.readthedocs.io/en/master/guide/rl_tips.html for more information.",
     ]
 ]
 
