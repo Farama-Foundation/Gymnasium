b14591ec3cabe3086d856147cf5154f3df5df31a
ChristofKaufmann <christof.kaufmann@hs-bochum.de>
2023-04-25 03:47:51 -0700

    Skip Jax and PyTorch tests if module is missing (#466)

--- tests/experimental/wrappers/test_init_shorten_import.py
+++ tests/experimental/wrappers/test_init_shorten_import.py
@@ -10,7 +10,7 @@ from gymnasium.experimental.wrappers import __all__
 
 
 def test_all_wrapper_shorten():
-    """Test that all wrappers in `__alL__` are contained within the `_wrapper_to_class` conversion."""
+    """Test that all wrappers in `__all__` are contained within the `_wrapper_to_class` conversion."""
     all_wrappers = set(__all__)
     all_wrappers.remove("vector")
     assert all_wrappers == set(_wrapper_to_class.keys())
@@ -18,6 +18,9 @@ def test_all_wrapper_shorten():
 
 @pytest.mark.parametrize("wrapper_name", __all__)
 def test_all_wrappers_shortened(wrapper_name):
-    """Check that each element of the `__all__` wrappers can be loaded."""
+    """Check that each element of the `__all__` wrappers can be loaded, provided dependencies are installed."""
     if wrapper_name != "vector":
-        assert getattr(gymnasium.experimental.wrappers, wrapper_name) is not None
+        try:
+            assert getattr(gymnasium.experimental.wrappers, wrapper_name) is not None
+        except gymnasium.error.DependencyNotInstalled as e:
+            pytest.skip(str(e))
