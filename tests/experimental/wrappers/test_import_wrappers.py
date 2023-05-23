39a929cd5b006bbbb0639690af2c46dd5c7d0a1e
pseudo-rnd-thoughts <mark.m.towers@gmail.com>
2023-05-23 14:52:07 +0100

    Misc bug fixes

--- tests/experimental/wrappers/test_import_wrappers.py
+++ tests/experimental/wrappers/test_import_wrappers.py
@@ -4,7 +4,12 @@ import re
 
 import pytest
 
+import gymnasium
 import gymnasium.experimental.wrappers as wrappers
+from gymnasium.experimental.wrappers import (
+    _wrapper_to_class,  # pyright: ignore[reportPrivateUsage]
+)
+from gymnasium.experimental.wrappers import __all__
 
 
 def test_import_wrappers():
@@ -41,3 +46,24 @@ def test_import_wrappers():
         ),
     ):
         getattr(wrappers, "NonexistentWrapper")
+
+
+def test_all_wrapper_shorten():
+    """Test that all wrappers in `__all__` are contained within the `_wrapper_to_class` conversion."""
+    all_wrappers = set(__all__)
+    all_wrappers.remove("vector")
+    assert all_wrappers == set(_wrapper_to_class.keys())
+
+
+@pytest.mark.parametrize("wrapper_name", __all__)
+def test_all_wrappers_shortened(wrapper_name):
+    """Check that each element of the `__all__` wrappers can be loaded, provided dependencies are installed."""
+    if wrapper_name != "vector":
+        try:
+            assert getattr(gymnasium.experimental.wrappers, wrapper_name) is not None
+        except gymnasium.error.DependencyNotInstalled as e:
+            pytest.skip(str(e))
+
+
+def test_wrapper_vector():
+    assert gymnasium.experimental.wrappers.vector is not None
