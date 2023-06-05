5d966eb46dbce88f4f28aa2c7ff457a5cf722754
Kallinteris Andreas <30759571+Kallinteris-Andreas@users.noreply.github.com>
2023-06-05 16:54:40 +0300

    Update test_mujoco_custom_env.py

--- tests/envs/mujoco/test_mujoco_custom_env.py
+++ tests/envs/mujoco/test_mujoco_custom_env.py
@@ -108,6 +108,6 @@ def test_frame_skip(frame_skip):
 
 def test_xml_file():
     """Verify that the loading of a custom XML file works"""
-    env = PointEnv(xml_file=os.path.abspath(os.getcwd()))
+    env = PointEnv(xml_file=os.path.abspath(os.getcwd()) + '------')
     env = PointEnv(xml_file=".tests/envs/mujoco/assets/walker2d_v5_uneven_feet.xml")
     assert env.unwrapped.data.qpos.size == 9
