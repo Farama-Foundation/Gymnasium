79d15d9f13c496308c63b32e3f894d9c241ca574
Reggie McLean <reginald.mclean@ryerson.ca>
2023-07-10 12:29:28 -0400

    removing sync env check obs space

--- gymnasium/vector/sync_vector_env.py
+++ gymnasium/vector/sync_vector_env.py
@@ -242,12 +242,12 @@ class SyncVectorEnv(VectorEnv):
 
     def _check_spaces(self) -> bool:
         for env in self.envs:
-            if not (env.observation_space == self.single_observation_space):
+            '''if not (env.observation_space == self.single_observation_space):
                 raise RuntimeError(
                     "Some environments have an observation space different from "
                     f"`{self.single_observation_space}`. In order to batch observations, "
                     "the observation spaces from all environments must be equal."
-                )
+                )'''
 
             if not (env.action_space == self.single_action_space):
                 raise RuntimeError(
