61d0848b741764d07dcb97fca4300d51b647c844
Kallinteris Andreas <30759571+Kallinteris-Andreas@users.noreply.github.com>
2023-10-23 18:29:19 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -31,7 +31,7 @@ def get_description():
                 long_description += line
             else:
                 break
-    return long_description
+    return long_descriptiona
 
 
 setup(name="gymnasium", version=get_version(), long_description=get_description())
