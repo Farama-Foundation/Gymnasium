e85ac98da7f7fc5a60dea1ccec5179460c0b2661
Mark Towers <mark.m.towers@gmail.com>
2023-12-17 15:51:58 +0000

    Update test_frame_stack_observation.py

--- tests/wrappers/test_frame_stack_observation.py
+++ tests/wrappers/test_frame_stack_observation.py
@@ -1,19 +1,19 @@
 """Test suite for FrameStackObservation wrapper."""
 import re
 
+import numpy as np
 import pytest
 
 import gymnasium as gym
 from gymnasium.utils.env_checker import data_equivalence
 from gymnasium.vector.utils import iterate
 from gymnasium.wrappers import FrameStackObservation
-from gymnasium.wrappers.utils import create_zero_array
 from tests.wrappers.utils import SEED, TESTING_OBS_ENVS, TESTING_OBS_ENVS_IDS
 
 
 @pytest.mark.parametrize("env", TESTING_OBS_ENVS, ids=TESTING_OBS_ENVS_IDS)
-def test_env_obs(env, stack_size: int = 3):
-    """Test different environment observations for testing."""
+def test_different_obs_spaces(env, stack_size: int = 3):
+    """Test across a large number of observation spaces to check if the FrameStack wrapper ."""
     obs, _ = env.reset(seed=SEED)
     env.action_space.seed(SEED)
 
@@ -73,36 +73,50 @@ def test_stack_size(stack_size: int):
         frames = list(iterate(env.observation_space, stacked_obs[i]))
         for j in range(stack_size):
             if i - j < 0:
-                expected_obs = first_obs  # Use the first observation instead of a zero observation
+                # Use the first observation instead of a zero observation
+                expected_obs = first_obs
             else:
                 expected_obs = unstacked_obs[i - j]
             assert data_equivalence(expected_obs, frames[stack_size - 1 - j])
 
 
-@pytest.mark.parametrize("stack_size", [2, 3, 4])
-def test_stack_size_zero_padding(stack_size: int):
-    """Test different stack sizes for FrameStackObservation wrapper."""
+def test_padding_type():
     env = gym.make("CartPole-v1")
-    env.action_space.seed(seed=SEED)
-    first_obs, _ = env.reset(seed=SEED)
-    second_obs, _, _, _, _ = env.step(env.action_space.sample())
+    reset_obs, _ = env.reset(seed=123)
+    action = env.action_space.sample()
+    step_obs, _, _, _, _ = env.step(action)
 
-    zero_obs = create_zero_array(env.observation_space)
+    stacked_env = FrameStackObservation(env, stack_size=3)  # default = "reset"
+    stacked_obs, _ = stacked_env.reset(seed=123)
+    assert np.all(np.stack([reset_obs, reset_obs, reset_obs]) == stacked_obs)
+    stacked_obs, _, _, _, _ = stacked_env.step(action)
+    assert np.all(np.stack([reset_obs, reset_obs, step_obs]) == stacked_obs)
 
-    env = FrameStackObservation(env, stack_size=stack_size, padding="zero")
+    stacked_env = FrameStackObservation(env, stack_size=3, padding_type="zero")
+    stacked_obs, _ = stacked_env.reset(seed=123)
+    assert np.all(np.stack([np.zeros(4), np.zeros(4), reset_obs]) == stacked_obs)
+    stacked_obs, _, _, _, _ = stacked_env.step(action)
+    assert np.all(np.stack([np.zeros(4), reset_obs, step_obs]) == stacked_obs)
 
-    env.action_space.seed(seed=SEED)
-    obs, _ = env.reset(seed=SEED)
-    unstacked_obs = list(iterate(env.observation_space, obs))
-    assert len(unstacked_obs) == stack_size
-    assert data_equivalence(
-        [zero_obs for _ in range(stack_size - 1)], unstacked_obs[:-1]
+    stacked_env = FrameStackObservation(
+        env, stack_size=3, padding_type=np.array([1, -1, 0, 2], dtype=np.float32)
     )
-    assert data_equivalence(first_obs, unstacked_obs[-1])
-
-    obs, _, _, _, _ = env.step(env.action_space.sample())
-    unstacked_obs = list(iterate(env.observation_space, obs))
-    assert data_equivalence(second_obs, unstacked_obs[-1])
+    stacked_obs, _ = stacked_env.reset(seed=123)
+    assert np.all(
+        np.stack(
+            [
+                np.array([1, -1, 0, 2], dtype=np.float32),
+                np.array([1, -1, 0, 2], dtype=np.float32),
+                reset_obs,
+            ]
+        )
+        == stacked_obs
+    )
+    stacked_obs, _, _, _, _ = stacked_env.step(action)
+    assert np.all(
+        np.stack([np.array([1, -1, 0, 2], dtype=np.float32), reset_obs, step_obs])
+        == stacked_obs
+    )
 
 
 def test_stack_size_failures():
@@ -115,10 +129,28 @@ def test_stack_size_failures():
             "The stack_size is expected to be an integer, actual type: <class 'float'>"
         ),
     ):
+        FrameStackObservation(env, stack_size=1.0)
+
+    with pytest.raises(
+        ValueError,
+        match=re.escape("The stack_size needs to be greater than one, actual value: 1"),
+    ):
         FrameStackObservation(env, stack_size=1)
 
     with pytest.raises(
         ValueError,
-        match=re.escape("The stack_size needs to be greater than one, actual value: 0"),
+        match=re.escape(
+            "Unexpected `padding_type`, expected 'reset', 'zero' or a custom observation space, actual value: 'unknown'"
+        ),
     ):
-        FrameStackObservation(env, stack_size=0)
+        FrameStackObservation(env, stack_size=3, padding_type="unknown")
+
+    invalid_padding = np.array([1, 2, 3, 4, 5])
+    assert invalid_padding not in env.observation_space
+    with pytest.raises(
+        ValueError,
+        match=re.escape(
+            "Unexpected `padding_type`, expected 'reset', 'zero' or a custom observation space, actual value: array([1, 2, 3, 4, 5])"
+        ),
+    ):
+        FrameStackObservation(env, stack_size=3, padding_type=invalid_padding)
