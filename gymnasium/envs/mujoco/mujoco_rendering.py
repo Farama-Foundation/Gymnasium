47071636bb6ab1c932840936a164b6c7044820e1
Kallinteris Andreas <30759571+Kallinteris-Andreas@users.noreply.github.com>
2024-01-13 19:52:04 +0200

    Fix MuJoCo human rendering on `mujoco<3` (#879)

--- gymnasium/envs/mujoco/mujoco_rendering.py
+++ gymnasium/envs/mujoco/mujoco_rendering.py
@@ -619,9 +619,6 @@ class WindowViewer(BaseRender):
                 bottomleft, "Solver iterations", str(self.data.solver_iter + 1)
             )
         self.add_overlay(
-            bottomleft, "Solver iterations", str(self.data.solver_niter[0] + 1)
-        )
-        self.add_overlay(
             bottomleft, "Step", str(round(self.data.time / self.model.opt.timestep))
         )
         self.add_overlay(bottomleft, "timestep", "%.5f" % self.model.opt.timestep)
