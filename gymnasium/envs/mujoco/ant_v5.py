136d1e135ec9b53d5a8dc3aa9a9f2815d9bc1428
Kallinteris Andreas <30759571+Kallinteris-Andreas@users.noreply.github.com>
2024-02-05 11:54:30 +0200

    [MuJoCo] [Documention] specify the size of the ant in it's description (#908)

--- gymnasium/envs/mujoco/ant_v5.py
+++ gymnasium/envs/mujoco/ant_v5.py
@@ -21,6 +21,7 @@ class AntEnv(MujocoEnv, utils.EzPickle):
     The ant is a 3D quadruped robot consisting of a torso (free rotational body) with four legs attached to it, where each leg has two body parts.
     The goal is to coordinate the four legs to move in the forward (right) direction by applying torque to the eight hinges connecting the two body parts of each leg and the torso (nine body parts and eight hinges).
 
+    Note: Although the robot is called "Ant", it is actually 75cm tall and weighs 910.88g.
 
     ## Action Space
     ```{figure} action_space_figures/ant.png
