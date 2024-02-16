833dbafa047e9317c5f452e51f5ef3ba46fe37f0
Kallinteris Andreas <30759571+Kallinteris-Andreas@users.noreply.github.com>
2024-02-16 08:22:32 +0200

    Update test_mujoco_v5.py

--- tests/envs/mujoco/test_mujoco_v5.py
+++ tests/envs/mujoco/test_mujoco_v5.py
@@ -588,7 +588,8 @@ def test_model_object_count(version: str):
     assert env.model.nv == 11
     assert env.model.nu == 7
     assert env.model.nbody == 13
-    assert env.model.nbvh == 18
+    if mujoco.__version__ >= "3.1.2"
+        assert env.model.nbvh == 8
     assert env.model.njnt == 11
     assert env.model.ngeom == 21
     assert env.model.ntendon == 0
@@ -599,7 +600,8 @@ def test_model_object_count(version: str):
     assert env.model.nv == 4
     assert env.model.nu == 2
     assert env.model.nbody == 5
-    assert env.model.nbvh == 5
+    if mujoco.__version__ >= "3.1.2"
+        assert env.model.nbvh == 3
     assert env.model.njnt == 4
     assert env.model.ngeom == 10
     assert env.model.ntendon == 0
@@ -610,7 +612,8 @@ def test_model_object_count(version: str):
     assert env.model.nv == 5
     assert env.model.nu == 2
     assert env.model.nbody == 4
-    assert env.model.nbvh == 4
+    if mujoco.__version__ >= "3.1.2"
+        assert env.model.nbvh == 0
     assert env.model.njnt == 5
     assert env.model.ngeom == 4
     assert env.model.ntendon == 0
