e3561b970a19a326198b60b63989736466b13e67
Kallinteris Andreas <30759571+Kallinteris-Andreas@users.noreply.github.com>
2024-02-16 11:00:43 +0200

    add `exact` argument to `utlis.env_checker.data_equivilance` (#924)

--- gymnasium/utils/env_checker.py
+++ gymnasium/utils/env_checker.py
@@ -30,37 +30,42 @@ from gymnasium.utils.passive_env_checker import (
 )
 
 
-def data_equivalence(data_1, data_2) -> bool:
+def data_equivalence(data_1, data_2, exact: bool = False) -> bool:
     """Assert equality between data 1 and 2, i.e observations, actions, info.
 
     Args:
         data_1: data structure 1
         data_2: data structure 2
+        exact: whether to compare array exactly or not if false compares with absolute and realive torrelance of 1e-5 (for more information check [np.allclose](https://numpy.org/doc/stable/reference/generated/numpy.allclose.html)).
 
     Returns:
         If observation 1 and 2 are equivalent
     """
-    if type(data_1) is type(data_2):
-        if isinstance(data_1, dict):
-            return data_1.keys() == data_2.keys() and all(
-                data_equivalence(data_1[k], data_2[k]) for k in data_1.keys()
-            )
-        elif isinstance(data_1, (tuple, list)):
-            return len(data_1) == len(data_2) and all(
-                data_equivalence(o_1, o_2) for o_1, o_2 in zip(data_1, data_2)
-            )
-        elif isinstance(data_1, np.ndarray):
-            if data_1.shape == data_2.shape and data_1.dtype == data_2.dtype:
-                if data_1.dtype == object:
-                    return all(data_equivalence(a, b) for a, b in zip(data_1, data_2))
-                else:
-                    return np.allclose(data_1, data_2, atol=0.00001)
-            else:
-                return False
-        else:
-            return data_1 == data_2
-    else:
+    if type(data_1) is not type(data_2):
         return False
+    if isinstance(data_1, dict):
+        return data_1.keys() == data_2.keys() and all(
+            data_equivalence(data_1[k], data_2[k], exact) for k in data_1.keys()
+        )
+    elif isinstance(data_1, (tuple, list)):
+        return len(data_1) == len(data_2) and all(
+            data_equivalence(o_1, o_2, exact) for o_1, o_2 in zip(data_1, data_2)
+        )
+    elif isinstance(data_1, np.ndarray):
+        if data_1.shape == data_2.shape and data_1.dtype == data_2.dtype:
+            if data_1.dtype == object:
+                return all(
+                    data_equivalence(a, b, exact) for a, b in zip(data_1, data_2)
+                )
+            else:
+                if exact:
+                    return np.all(data_1 == data_2)
+                else:
+                    return np.allclose(data_1, data_2, rtol=1e-5, atol=1e-5)
+        else:
+            return False
+    else:
+        return data_1 == data_2
 
 
 def check_reset_seed(env: gym.Env):
