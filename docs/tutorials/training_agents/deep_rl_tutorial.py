e89873b00282f19fb022ff456160ec848196191e
Hardy Hasan <Harady.Hasan@gmail.com>
2024-02-20 12:06:54 +0300

    Starting with implementation of the training function.

--- docs/tutorials/training_agents/deep_rl_tutorial.py
+++ docs/tutorials/training_agents/deep_rl_tutorial.py
@@ -18,9 +18,10 @@ Let's start by importing necessary libraries:
 
 # %%
 __author__ = "Hardy Hasan"
-__date__ = "2023-02-13"
+__date__ = "2023-02-20"
 __license__ = "MIT License"
 
+import copy
 import random
 from collections import namedtuple
 from typing import Tuple
@@ -29,20 +30,11 @@ import numpy as np
 import torch
 import torch.nn as nn
 
-
-# import torch.nn as nn
-
-# import gymnasium as gym
+import gymnasium
 
 
 # utilize gpu if available
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-
-
-def set_seed(seed):
-    # TODO: implement this function to seed all random libraries such as random, numpy, torch.
-    pass
-
 
 # %%
 # Skeleton
@@ -60,6 +52,54 @@ def set_seed(seed):
 #
 # Let's start by implementing the memory and agent:
 
+
+# %%
+# Helper functions/classes
+# ------------------------
+# All the functions we need to perform small but repetitive tasks
+# that are not related to the agent/training logic.
+class Results:
+    """
+    This class stores results from episodes of agent-environment interaction and
+    uses these results for reporting progress or overall agent performance.
+    """
+
+    __slots__ = []
+
+    def __init__(self):
+        pass
+
+
+def to_tensor(array: np.ndarray, normalize: bool = False) -> torch.Tensor:
+    """
+    Takes any array-like object and turns it into a torch.Tensor on `device`.
+    For atari image observations, the normalize parameter can be used to change
+    the range from [0,255) to [0,1).
+
+    Args:
+        array: An array, which can be states, actions, rewards, etc.
+        normalize: Whether to normalize image observations
+
+    Returns:
+        tensor
+    """
+    tensor = torch.tensor(array, device=device)
+
+    if normalize:
+        return tensor / 255.0
+    return tensor
+
+
+def set_seed(seed):
+    # TODO: implement this function to seed all random libraries such as random, numpy, torch.
+    pass
+
+
+def create_atari_env(env: gymnasium.Env) -> gymnasium.Env:
+    # TODO: implement function and write doc
+    return env
+
+
 # %%
 # Agent
 # -----
@@ -72,57 +112,56 @@ class ReplayMemory:
     Buffer for experience storage.
 
     This implementation uses numpy arrays for storing experiences, and allocating
-    required RAM upfront instead of storing dynamically. This will enable us to know
+    required RAM upfront instead of storing dynamically. This enables us to know
     upfront whether enough memory is available on the machine, insteaf of the training
     being quit unexpectedly.
-    Each term in an experience (state, action , next_state, reward ,done) is stored separately
-    into a different array. A maximum capacity is required upon initialization, as well as
+    Each term in an experience (state, action , next_state, reward ,done) is stored into
+    separate arrays. A maximum capacity is required upon initialization, as well as
     observation shape, and whether the observation is image data, such as in Atari games.
 
     In Atari games, the frame stacking technique is used, where the past four observations make
     up a state. Thus, for each experience, `state` and `next_state` are four frames each, however
     the first three frames of `next_state` is the last three frames of `state`, hence these frames
-    are stored once in the states array, and when sampling, reconcatenated back to build a proper
-    `next_state`.
+    are stored once in the `next_states` array, and when sampling, reconcatenated back to build a
+    proper `next_state`.
     """
 
-    def __init__(
-        self,
-        capacity: int,
-        batch_size: int,
-        obs_shape: Tuple[int, int],
-        image_obs: bool,
-        frame_stacking: int = 4,
-    ) -> None:
+    def __init__(self, params: namedtuple) -> None:
         """
         Initialize a replay memory.
+
+        Args:
+            params: A namedtuple containing all hperparameters needed for training an agent,
+                    hence it contains all the parameters needed for creating a memory buffer.
         """
-        self.capacity = capacity
-        self.batch_size = batch_size
-        self.image_obs = image_obs
+        self.params = params
         self.length: int = 0  # number of experiences stored so far
         self.index: int = 0  # current index to store data to
 
         # shape of state buffers differ depending on whether an obs is image data.
-        if image_obs:
-            self._state_shape = (self.capacity, frame_stacking, *obs_shape)
+        if self.params.image_obs:
+            self._state_shape = (
+                self.params.capacity,
+                self.params.frame_stacking,
+                *self.params.obs_shape,
+            )
             self._next_state_shape = (
-                self.capacity,
+                self.params.capacity,
                 1,
-                *obs_shape,
-            )  # storing one frame for next_state
+                *self.params.obs_shape,
+            )
         else:
-            self._state_shape = (self.capacity, *obs_shape)
-            self._next_state_shape = (self.capacity, *obs_shape)
+            self._state_shape = (self.params.capacity, *self.params.obs_shape)
+            self._next_state_shape = (self.params.capacity, *self.params.obs_shape)
 
         # creating the buffers
         self._states: np.ndarray = np.zeros(shape=self._state_shape, dtype=np.float16)
-        self._actions: np.ndarray = np.zeros(self.capacity, dtype=np.uint8)
+        self._actions: np.ndarray = np.zeros(self.params.capacity, dtype=np.uint8)
         self._next_states: np.ndarray = np.zeros(
             shape=self._next_state_shape, dtype=np.float16
         )
-        self._rewards: np.ndarray = np.zeros(self.capacity, dtype=np.float16)
-        self._dones: np.ndarray = np.zeros(self.capacity, dtype=np.uint8)
+        self._rewards: np.ndarray = np.zeros(self.params.capacity, dtype=np.float16)
+        self._dones: np.ndarray = np.zeros(self.params.capacity, dtype=np.uint8)
 
     def __len__(self):
         """Returns the length of the memory."""
@@ -136,99 +175,58 @@ class ReplayMemory:
         reward: float,
         done: bool,
     ) -> None:
-        """Adds a new experience into the buffer"""
+        """
+        Adds a new experience into the buffer
+
+        Args:
+            state: Current agent state.
+            action: The taken action at `state`.
+            next_state: The resulting state from taking action.
+            reward: Reward signal.
+            done: Whether episode endeed after taking action.
+
+        Returns:
+
+        """
         self._states[self.index] = state
         self._actions[self.index] = action
         self._next_states[self.index] = next_state
         self._rewards[self.index] = reward
         self._dones[self.index] = done
 
-        self.length = min(self.length + 1, self.capacity)
-        self.index = (self.index + 1) % self.capacity
+        self.length = min(self.length + 1, self.params.capacity)
+        self.index = (self.index + 1) % self.params.capacity
 
         return
 
     def sample(
         self,
-    ) -> Tuple[torch.Tensor, torch.Tensor, torch.Tensor, torch.Tensor, torch.Tensor]:
+    ) -> Tuple[np.ndarray, np.ndarray, np.ndarray, np.ndarray, np.ndarray]:
         """
-        Samples a random batch of experiences and returns them as torch.Tensors.
+        Samples and returns a random batch of experiences.
         """
         indices = np.random.choice(
-            a=np.arange(self.length), size=self.batch_size, replace=False
+            a=np.arange(self.length), size=self.params.batch_size, replace=False
         )
 
         states = self._states[indices]
         next_states = self._next_states[indices]
         next_states = (
-            np.concatenate((states[:, 1:, :, :], next_states), axis=1)
-            if self.image_obs
-            else self._next_states[indices]
+            self._next_states[indices]
+            if self.params.image_obs
+            else np.concatenate((states[:, 1:, :, :], next_states), axis=1)
         )
 
-        states = torch.tensor(states, dtype=torch.float, device=device)
-        next_states = torch.tensor(next_states, dtype=torch.float, device=device)
-        actions = torch.tensor(
-            self._actions[indices], dtype=torch.int64, device=device
-        ).view(-1, 1)
-        rewards = torch.tensor(
-            self._rewards[indices], dtype=torch.float, device=device
-        ).view(-1, 1)
-        dones = torch.tensor(self._dones[indices], dtype=torch.int, device=device).view(
-            -1, 1
-        )
+        actions = self._actions[indices]
+        rewards = self._rewards[indices]
+        dones = self._dones[indices]
 
-        if self.image_obs:
+        if self.params.image_obs:
             assert torch.equal(
                 states[:, 1:, :], next_states[:, :3, :]
             ), "Incorrect concatenation."
 
         return states, actions, next_states, rewards, dones
-
-
-# %%
-# Test ReplayMemory
-# This section can be removed later when everything is done. It is here for
-# debugging purposes only.
-
-# test 1: array obs
-test1_seed = 111
-np.random.seed(test1_seed)
-capacity = 5
-batch_size = 3
-obs_shape = (1, 3)
-image_obs = False
-mem = ReplayMemory(
-    capacity=capacity, batch_size=batch_size, obs_shape=obs_shape, image_obs=image_obs
-)
-states = [np.random.random((1, 3)) for _ in range(capacity + 1)]
-actions = [np.random.randint(10) for _ in range(capacity + 1)]
-next_states = [np.random.random((1, 3)) for _ in range(capacity + 1)]
-rewards = [np.random.random() for _ in range(capacity + 1)]
-dones = [[True, False][i % 2] for i in range(capacity + 1)]
-# make sure that each obs goes into right position, and length and index are correctly updated.
-for i in range(capacity + 1):
-    mem.push(states[i], actions[i], next_states[i], rewards[i], dones[i])
-# make sure that sapling a batch returns correct dimensions
-batch = mem.sample()
-
-# test 2: image obs
-test1_seed = 111
-np.random.seed(test1_seed)
-capacity = 5
-batch_size = 3
-obs_shape = (3, 3)
-image_obs = True
-mem = ReplayMemory(
-    capacity=capacity, batch_size=batch_size, obs_shape=obs_shape, image_obs=image_obs
-)
-states = [np.random.random((4, 3, 3)) for _ in range(capacity + 1)]
-next_states = [np.random.random((1, 3, 3)) for _ in range(capacity + 1)]
-
-for i in range(capacity + 1):
-    mem.push(states[i], actions[i], next_states[i], rewards[i], dones[i])
-# make sure that sapling a batch returns correct dimensions
-batch = mem.sample()
 
 
 class Agent(nn.Module):
@@ -248,21 +246,17 @@ class Agent(nn.Module):
         """
         super().__init__(params)
         self.params = params
-        # TODO: Fix the epsilon reduction strategy
-        self.epsilon = 0
+        self.epsilon = self.params.eps_start
+        self.eps_reduction = (self.params.eps_start - self.params.eps_end) / (
+            self.params.anneal_length_percent * self.params.training_steps
+        )
 
         self.reduce_eps_steps = (
             self.params.eps_start - self.params.eps_end
         ) / self.params.anneal_length
         self.delta = (self.params.v_max - self.params.v_min) / (self.params.n_atoms - 1)
 
-        self.replay_memory = ReplayMemory(
-            capacity=self.params.memory_capacity,
-            batch_size=self.params.batch_size,
-            obs_shape=self.params.obs_shape,
-            image_obs=self.params.image_obs,
-            frame_stacking=self.params.frame_stacking,
-        )
+        self.replay_memory = ReplayMemory(self.params)
 
         self.optimizer = torch.optim.Adam(
             params=self.parameters(), lr=self.params.learning_rate
@@ -304,9 +298,7 @@ class Agent(nn.Module):
                 padding=(0, 0),
             )
         else:
-            self.conv1 = None
-            self.conv2 = None
-            self.conv3 = None
+            self.conv1, self.conv2, self.conv3 = None, None, None
 
         self.fc1 = nn.Linear(in_features=in_features, out_features=n_hidden_units)
         self.fc2 = nn.Linear(in_features=n_hidden_units, out_features=out_features)
@@ -352,6 +344,9 @@ class Agent(nn.Module):
         """
         random_value = random.random()
 
+        if self.epsilon > self.params.eps_end:
+            self.epsilon -= self.eps_reduction
+
         with torch.no_grad():
             value_dist = self.forward(state)
 
@@ -367,12 +362,7 @@ class Agent(nn.Module):
         return action
 
     def store_experience(
-        self,
-        state: torch.Tensor,
-        action: int,
-        next_state: torch.Tensor,
-        reward: float,
-        done: bool,
+        self, state, action: int, next_state, reward: float, done: bool
     ):
         """
 
@@ -386,6 +376,7 @@ class Agent(nn.Module):
         Returns:
 
         """
+        # TODO: fix the type hints for the function parameters state and next_state
         self.replay_memory.push(
             state=state, action=action, next_state=next_state, reward=reward, done=done
         )
@@ -402,6 +393,12 @@ class Agent(nn.Module):
                   of states.
         """
         states, actions, next_states, rewards, dones = self.replay_memory.sample()
+
+        states = to_tensor(array=states, normalize=self.params.image_obs)
+        actions = to_tensor(array=actions).view(-1, 1)
+        next_states = to_tensor(array=next_states, normalize=self.params.image_obs)
+        rewards = to_tensor(array=rewards).view(-1, 1)
+        dones = to_tensor(array=dones).view(-1, 1)
 
         # agent predictions
         value_dists = self.forward(states)
@@ -424,11 +421,11 @@ class Agent(nn.Module):
         next_actions = ((self.forward(next_states) * self.support).sum(dim=2)).argmax(
             dim=1
         )
-        target_probs = target_value_dists[torch.arange(batch_size), next_actions, :]
+        target_probs = target_value_dists[
+            torch.arange(self.params.batch_size), next_actions, :
+        ]
 
-        m = torch.zeros(self.params.batch_size * self.params.n_atoms).to(
-            device
-        )  # distributing probabilities in-place.
+        m = torch.zeros(self.params.batch_size * self.params.n_atoms).to(device)
 
         Tz = (rewards + self.params.gamma * self.support).clip(
             self.params.v_min, self.params.v_max
@@ -445,7 +442,7 @@ class Agent(nn.Module):
             )
             .long()
             .unsqueeze(1)
-            .expand(batch_size, self.N)
+            .expand(self.params.batch_size, self.N)
             .to(device)
         )
         m.index_add_(0, (l + offset).view(-1), (target_probs * (u - bj)).view(-1))
@@ -475,8 +472,71 @@ class Agent(nn.Module):
 # Describe useful statistics to plot during training to track agent progress.
 
 
-def train():
-    pass
+def train(params: namedtuple, seed: int, verbose: bool = True):
+    """
+    Creates agent and environment, and lets the agent interact
+    with the environment until it learns a good policy.
+
+    Args:
+        params: A namedtuple containing all necessary hyperparameters.
+        seed: For reprodicubility.
+        verbose: Whether to print training progress periodically.
+
+    Returns:
+
+    """
+    set_seed(seed)
+
+    steps = 0  # global time steps for the whole training
+
+    # results_collector = Results()
+
+    env = gymnasium.make(params.env_name)
+    if params.image_obs:
+        env = create_atari_env(env)
+
+    agent = Agent(params=params)
+    target_agent = copy.deepcopy(agent)
+    # Q_target parameters are frozen.
+    for p in target_agent.parameters():
+        p.requires_grad = False
+
+    while steps < params.training_steps:
+        done = False
+        obs, info = env.reset(seed=seed)
+
+        while not done:
+            state = to_tensor(obs, params.image_obs)
+            action = agent.act(state=state, exploit=False)
+            next_obs, reward, terminated, truncated, info = env.step(action)
+            done = terminated
+            agent.store_experience(
+                state=obs, action=action, next_state=next_obs, reward=reward, done=done
+            )
+            obs = next_obs
+
+            # TODO: store results
+
+            steps += 1
+
+            # train agent periodically if enough experience exists
+            if steps % params.update_frequency == 0 and (
+                len(agent.replay_memory) > params.batch_size
+            ):
+                _ = agent.learn(target_agent)
+
+            # Update the target network periodically.
+            if steps % params.target_update_frequency == 0:
+                target_agent.load_state_dict(agent.state_dict())
+
+            # print progress periodically
+            if verbose and steps % 10_000 == 0:
+                print(f"step={steps}/{params.training_steps}")
+
+    # TODO: clean up, create results summary, return stuff
+    env.close()
+
+    return
 
 
 # %%
@@ -505,10 +565,10 @@ Hyperparameters = namedtuple(
         "obs_shape",  # a tuple representing shape of observations, ex. (1, 4), (4, 84)
         "image_obs",  # boolean, indicating whether the env provides image observations
         "batch_size",  # number of experiences to sample for updating agent network parameters
-        "training_frequency",  # how often to update agent network parameters
+        "update_frequency",  # how often to update agent network parameters
         "target_update_frequency",  # how often to replace target agent network parameters
         "gamma",  # discount factor
-        "frame_stacking",
+        "frame_stacking",  # number of frames to be stacked together
         # --- exploration-exploitation strategy related ---
         "epsilon_start",
         "epsilon_end",
@@ -519,7 +579,7 @@ Hyperparameters = namedtuple(
         # --- optimizer related ---
         "learning_rate",
         # --- replay memory related ---
-        "memory_capacity",
+        "capacity",
         # --- agent algorithm related ---
         "v_min",
         "v_max",
@@ -552,6 +612,14 @@ env = None
 
 env2 = None
 
+# AtariPreprocessing
+# FrameStack
+# RecordEpisodeStatistics
+# 512 hidden units for atari
+# frame-skipping done by default in AtariPreprocessing
+
+# RecordVideo
+# how to evaluate a trained agent
 
 # %%
 # Finishing words
