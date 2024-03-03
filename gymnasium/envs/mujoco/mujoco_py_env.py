876e6a267aa180db4457a2f3fa54fabc64728fcf
Michael Panchenko <35432522+MischaPanch@users.noreply.github.com>
2024-03-03 13:24:47 +0100

    [Bug fix] remove `mujoco-py` import error for v4+ MuJoCo environments (#934)

--- gymnasium/envs/mujoco/mujoco_py_env.py
+++ gymnasium/envs/mujoco/mujoco_py_env.py
@@ -5,22 +5,29 @@ import numpy as np
 from numpy.typing import NDArray
 
 import gymnasium as gym
-from gymnasium import error, spaces
+from gymnasium import error, logger, spaces
 from gymnasium.spaces import Space
 
 
 try:
-    import mujoco
+    import mujoco_py
 except ImportError as e:
     raise error.DependencyNotInstalled(
-        "Could not import mujoco"
-        "(HINT: you need to install mujoco, run `pip install gymnasium[mujoco]`.)"
+        "Could not import mujoco_py, which is needed for MuJoCo environments older than V4",
+        "You could either use a newer version of the environments, or install the (deprecated) mujoco-py package"
+        "following the instructions on their GitHub page.",
     ) from e
 
 
+# NOTE: duplication of analogous code in mujoco_env.py
+# Support for mujoco-py based envs is deprecated, so this module will no longer be maintained
+# The code is duplicated instead of imported for it to be working in standalone and independent
+# of the further development of the maintained mujoco_env.py
+# noinspection DuplicatedCode
 DEFAULT_SIZE = 480
 
 
+# noinspection DuplicatedCode
 def expand_model_path(model_path: str) -> str:
     """Expands the `model_path` to a full path if it starts with '~' or '.' or '/'."""
     if model_path.startswith(".") or model_path.startswith("/"):
@@ -35,7 +42,8 @@ def expand_model_path(model_path: str) -> str:
     return fullpath
 
 
-class BaseMujocoEnv(gym.Env[NDArray[np.float64], NDArray[np.float32]]):
+# noinspection DuplicatedCode
+class BaseMujocoPyEnv(gym.Env[NDArray[np.float64], NDArray[np.float32]]):
     """Superclass for all MuJoCo environments."""
 
     def __init__(
@@ -194,22 +202,29 @@ class BaseMujocoEnv(gym.Env[NDArray[np.float64], NDArray[np.float32]]):
         return np.concatenate([self.data.qpos.flat, self.data.qvel.flat])
 
 
-class MujocoEnv(BaseMujocoEnv):
-    """Superclass for MuJoCo environments."""
-
+class MuJocoPyEnv(BaseMujocoPyEnv):
     def __init__(
         self,
-        model_path,
-        frame_skip,
-        observation_space: Optional[Space],
+        model_path: str,
+        frame_skip: int,
+        observation_space: Space,
         render_mode: Optional[str] = None,
         width: int = DEFAULT_SIZE,
         height: int = DEFAULT_SIZE,
         camera_id: Optional[int] = None,
         camera_name: Optional[str] = None,
-        default_camera_config: Optional[Dict[str, Union[float, int]]] = None,
-        max_geom: int = 1000,
     ):
+        logger.deprecation(
+            "This version of the mujoco environments depends "
+            "on the mujoco-py bindings, which are no longer maintained "
+            "and may stop working. Please upgrade to the v5 or v4 versions of "
+            "the environments (which depend on the mujoco python bindings instead), unless "
+            "you are trying to precisely replicate previous works)."
+        )
+
+        self.viewer = None
+        self._viewers = {}
+
         super().__init__(
             model_path,
             frame_skip,
@@ -221,56 +236,107 @@ class MujocoEnv(BaseMujocoEnv):
             camera_name,
         )
 
-        from gymnasium.envs.mujoco.mujoco_rendering import MujocoRenderer
-
-        self.mujoco_renderer = MujocoRenderer(
-            self.model,
-            self.data,
-            default_camera_config,
-            self.width,
-            self.height,
-            max_geom,
-            camera_id,
-            camera_name,
-        )
-
-    def _initialize_simulation(
-        self,
-    ) -> Tuple["mujoco._structs.MjModel", "mujoco._structs.MjData"]:
-        model = mujoco.MjModel.from_xml_path(self.fullpath)
-        # MjrContext will copy model.vis.global_.off* to con.off*
-        model.vis.global_.offwidth = self.width
-        model.vis.global_.offheight = self.height
-        data = mujoco.MjData(model)
+    def _initialize_simulation(self):
+        model = mujoco_py.load_model_from_path(self.fullpath)
+        self.sim = mujoco_py.MjSim(model)
+        data = self.sim.data
         return model, data
 
     def _reset_simulation(self):
-        mujoco.mj_resetData(self.model, self.data)
+        self.sim.reset()
 
     def set_state(self, qpos, qvel):
         super().set_state(qpos, qvel)
-        self.data.qpos[:] = np.copy(qpos)
-        self.data.qvel[:] = np.copy(qvel)
-        if self.model.na == 0:
-            self.data.act[:] = None
-        mujoco.mj_forward(self.model, self.data)
-
-    def _step_mujoco_simulation(self, ctrl, n_frames):
-        self.data.ctrl[:] = ctrl
-
-        mujoco.mj_step(self.model, self.data, nstep=n_frames)
-
-        # As of MuJoCo 2.0, force-related quantities like cacc are not computed
-        # unless there's a force sensor in the model.
-        # See https://github.com/openai/gym/issues/1541
-        mujoco.mj_rnePostConstraint(self.model, self.data)
-
-    def render(self):
-        return self.mujoco_renderer.render(self.render_mode)
-
-    def close(self):
-        if self.mujoco_renderer is not None:
-            self.mujoco_renderer.close()
+        state = self.sim.get_state()
+        state = mujoco_py.MjSimState(state.time, qpos, qvel, state.act, state.udd_state)
+        self.sim.set_state(state)
+        self.sim.forward()
 
     def get_body_com(self, body_name):
-        return self.data.body(body_name).xpos
+        return self.data.get_body_xpos(body_name)
+
+    def _step_mujoco_simulation(self, ctrl, n_frames):
+        self.sim.data.ctrl[:] = ctrl
+
+        for _ in range(n_frames):
+            self.sim.step()
+
+    def render(self):
+        if self.render_mode is None:
+            assert self.spec is not None
+            gym.logger.warn(
+                "You are calling render method without specifying any render mode. "
+                "You can specify the render_mode at initialization, "
+                f'e.g. gym.make("{self.spec.id}", render_mode="rgb_array")'
+            )
+            return
+
+        width, height = self.width, self.height
+        camera_name, camera_id = self.camera_name, self.camera_id
+        if self.render_mode in {"rgb_array", "depth_array"}:
+            if camera_id is not None and camera_name is not None:
+                raise ValueError(
+                    "Both `camera_id` and `camera_name` cannot be"
+                    " specified at the same time."
+                )
+
+            no_camera_specified = camera_name is None and camera_id is None
+            if no_camera_specified:
+                camera_name = "track"
+
+            if camera_id is None and camera_name in self.model._camera_name2id:
+                if camera_name in self.model._camera_name2id:
+                    camera_id = self.model.camera_name2id(camera_name)
+
+                self._get_viewer(self.render_mode).render(
+                    width, height, camera_id=camera_id
+                )
+
+        if self.render_mode == "rgb_array":
+            data = self._get_viewer(self.render_mode).read_pixels(
+                width, height, depth=False
+            )
+            # original image is upside-down, so flip it
+            return data[::-1, :, :]
+        elif self.render_mode == "depth_array":
+            self._get_viewer(self.render_mode).render(width, height)
+            # Extract depth part of the read_pixels() tuple
+            data = self._get_viewer(self.render_mode).read_pixels(
+                width, height, depth=True
+            )[1]
+            # original image is upside-down, so flip it
+            return data[::-1, :]
+        elif self.render_mode == "human":
+            self._get_viewer(self.render_mode).render()
+
+    def _get_viewer(
+        self, mode
+    ) -> Union["mujoco_py.MjViewer", "mujoco_py.MjRenderContextOffscreen"]:
+        self.viewer = self._viewers.get(mode)
+        if self.viewer is None:
+            if mode == "human":
+                self.viewer = mujoco_py.MjViewer(self.sim)
+
+            elif mode in {"rgb_array", "depth_array"}:
+                self.viewer = mujoco_py.MjRenderContextOffscreen(self.sim, -1)
+            else:
+                raise AttributeError(
+                    f"Unknown mode: {mode}, expected modes: {self.metadata['render_modes']}"
+                )
+
+            self.viewer_setup()
+            self._viewers[mode] = self.viewer
+
+        return self.viewer
+
+    def close(self):
+        if self.viewer is not None:
+            self.viewer = None
+            self._viewers = {}
+
+    def viewer_setup(self):
+        """
+        This method is called when the viewer is initialized.
+        Optionally implement this method, if you need to tinker with camera position and so forth.
+        """
+        raise NotImplementedError
