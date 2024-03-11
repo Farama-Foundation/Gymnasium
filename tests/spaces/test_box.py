a9c1dcc7e802c58c8867fc56454abc925c742428
Mark Towers <mark.m.towers@gmail.com>
2024-03-11 22:57:11 +0000

    Fix issues in tests

--- tests/spaces/test_box.py
+++ tests/spaces/test_box.py
@@ -9,6 +9,46 @@ from gymnasium.spaces import Box
 
 
 @pytest.mark.parametrize(
+    "dtype, error, message",
+    [
+        (
+            None,
+            ValueError,
+            "Box dtype must be explicitly provided, cannot be None.",
+        ),
+        (0, TypeError, "Cannot interpret '0' as a data type"),
+        ("unknown", TypeError, "data type 'unknown' not understood"),
+        (np.zeros(1), TypeError, "Cannot construct a dtype from an array"),
+        # disabled datatypes
+        (
+            np.complex64,
+            ValueError,
+            "Invalid Box dtype (complex64), must be an integer, floating, or bool dtype",
+        ),
+        (
+            complex,
+            ValueError,
+            "Invalid Box dtype (complex128), must be an integer, floating, or bool dtype",
+        ),
+        (
+            object,
+            ValueError,
+            "Invalid Box dtype (object), must be an integer, floating, or bool dtype",
+        ),
+        (
+            str,
+            ValueError,
+            "Invalid Box dtype (<U0), must be an integer, floating, or bool dtype",
+        ),
+    ],
+)
+def test_dtype_errors(dtype, error, message):
+    """Test errors due to dtype mismatch either to being invalid or disallowed."""
+    with pytest.raises(error, match=re.escape(message)):
+        Box(low=0, high=1, dtype=dtype)
+
+
+@pytest.mark.parametrize(
     "box, expected_shape",
     [
         # Test with same 1-dim low and high shape
@@ -33,108 +73,113 @@ def test_shape_inference(box, expected_shape):
 
 
 @pytest.mark.parametrize(
-    "low, high, shape, message",
+    "low, high, shape, error_type, message",
     [
         (
             0,
             1,
+            1,
+            TypeError,
+            "Expected Box shape to be an iterable, actual type=<class 'int'>",
+        ),
+        (
+            0,
+            1,
             (None,),
-            "Expected all shape elements to be an integer, actual type: (<class 'NoneType'>,)",
+            TypeError,
+            "Expected all Box shape elements to be integer, actual type=(<class 'NoneType'>,)",
         ),
         (
             0,
             1,
             (1, None),
-            "Expected all shape elements to be an integer, actual type: (<class 'int'>, <class 'NoneType'>)",
+            TypeError,
+            "Expected all Box shape elements to be integer, actual type=(<class 'int'>, <class 'NoneType'>)",
         ),
         (
             0,
             1,
             (np.int64(1), None),
-            "Expected all shape elements to be an integer, actual type: (<class 'numpy.int64'>, <class 'NoneType'>)",
+            TypeError,
+            "Expected all Box shape elements to be integer, actual type=(<class 'numpy.int64'>, <class 'NoneType'>)",
         ),
         (
             np.zeros(3),
             np.ones(2),
             None,
-            "high.shape doesn't match provided shape, high.shape: (2,), shape: (3,)",
+            ValueError,
+            "Box low.shape and high.shape don't match, low.shape=(3,), high.shape=(2,)",
         ),
         (
             np.zeros(2),
             np.ones(2),
             (3,),
-            "low.shape doesn't match provided shape, low.shape: (2,), shape: (3,)",
+            ValueError,
+            "Box low.shape doesn't match provided shape, low.shape=(2,), shape=(3,)",
         ),
-    ],
-)
-def test_shape_errors(low, high, shape, message):
-    """Test errors due to shape mismatch."""
-    with pytest.raises(AssertionError, match=f"^{re.escape(message)}$"):
-        Box(low=low, high=high, shape=shape)
-
-
-@pytest.mark.parametrize(
-    "dtype, error, message",
-    [
         (
-            None,
-            AssertionError,
-            "Box dtype must be explicitly provided, cannot be None.",
+            np.zeros(2),
+            1,
+            (3,),
+            ValueError,
+            "Box low.shape doesn't match provided shape, low.shape=(2,), shape=(3,)",
         ),
-        (0, TypeError, "Cannot interpret '0' as a data type"),
-        ("unknown", TypeError, "data type 'unknown' not understood"),
-        (np.zeros(1), TypeError, "Cannot construct a dtype from an array"),
-        # disabled datatypes
-        (np.complex64, ValueError, "Invalid dtype (complex64) for Box"),
-        (complex, ValueError, "Invalid dtype (complex128) for Box"),
-        (object, ValueError, "Invalid dtype (object) for Box"),
-        (str, ValueError, "Invalid dtype (<U0) for Box"),
+        (
+            0,
+            np.ones(2),
+            (3,),
+            ValueError,
+            "Box high.shape doesn't match provided shape, high.shape=(2,), shape=(3,)",
+        ),
     ],
 )
-def test_dtype_errors(dtype, error, message):
-    """Test errors due to dtype mismatch either to being invalid or disallowed."""
-    with pytest.raises(error, match=re.escape(message)):
-        Box(low=0, high=1, dtype=dtype)
-
-
-def test_bool_warning():
-    """Test that using a bool dtype causes a warning."""
-    with pytest.warns(
-        UserWarning,
-        match=re.escape(
-            "Using `np.bool_` is not recommended with `Box`, `MultiBinary` is recommend for boolean samples."
-        ),
-    ):
-        Box(low=False, high=True, dtype=np.bool_)
-
-    with pytest.warns(
-        UserWarning,
-        match=re.escape(
-            "Using `bool` is not recommended with `Box`, `MultiBinary` is recommend for boolean samples."
-        ),
-    ):
-        Box(low=False, high=True, dtype=bool)
+def test_shape_errors(low, high, shape, error_type, message):
+    """Test errors due to shape mismatch."""
+    with pytest.raises(error_type, match=f"^{re.escape(message)}$"):
+        Box(low=low, high=high, shape=shape)
 
 
 @pytest.mark.parametrize(
     "low, high, dtype",
     [
         # floats
-        (0, 65501.0, np.float16),
-        (-65499.0, 0, np.float16),
+        (0, 65505.0, np.float16),
+        (-65505.0, 0, np.float16),
         # signed int
         (0, 32768, np.int16),
-        (-32767, 0, np.int16),
+        (-32769, 0, np.int16),
         # unsigned int
         (-1, 100, np.uint8),
         (0, 300, np.uint8),
-        (np.array([-1, 0]), np.array([0, 300]), np.uint8),
-        (np.array([[-1], [0]]), np.array([[0], [300]]), np.uint8),
+        # boolean
+        (-1, 1, np.bool_),
+        (0, 2, np.bool_),
+        # array inputs
+        (
+            np.array([-1, 0]),
+            np.array([0, 100]),
+            np.uint8,
+        ),
+        (
+            np.array([[-1], [0]]),
+            np.array([[0], [100]]),
+            np.uint8,
+        ),
+        (
+            np.array([0, 0]),
+            np.array([0, 300]),
+            np.uint8,
+        ),
+        (
+            np.array([[0], [0]]),
+            np.array([[0], [300]]),
+            np.uint8,
+        ),
     ],
 )
 def test_out_of_bounds_error(low, high, dtype):
     with pytest.raises(
-        ValueError, match="Box low or high value out of bounds of the dtype"
+        ValueError, match=re.escape("is out of bounds of the dtype range,")
     ):
         Box(low=low, high=high, dtype=dtype)
 
@@ -145,33 +190,42 @@ def test_out_of_bounds_error(low, high, dtype):
         # Floats
         (np.nan, 0, np.float32),
         (0, np.nan, np.float32),
-        ([0, np.nan], np.ones(2), np.float32),
+        (np.array([0, np.nan]), np.ones(2), np.float32),
         # Signed ints
         (np.nan, 0, np.int32),
         (0, np.nan, np.int32),
-        ([0, np.nan], np.ones(2), np.int32),
+        (np.array([0, np.nan]), np.ones(2), np.int32),
         # Unsigned ints
+        # (np.nan, 0, np.uint8),
+        # (0, np.nan, np.uint8),
+        # (np.array([0, np.nan]), np.ones(2), np.uint8),
         (-np.inf, 1, np.uint8),
-        ([-np.inf, 0], 1, np.uint8),
-        ([[-np.inf], [0]], 1, np.uint8),
+        (np.array([-np.inf, 0]), 1, np.uint8),
         (0, np.inf, np.uint8),
-        (0, [1, np.inf], np.uint8),
-        (0, [[1], [np.inf]], np.uint8),
-        (-np.inf, np.inf, np.uint8),
-        ([0, -np.inf], [np.inf, 1], np.uint8),
+        (0, np.array([1, np.inf]), np.uint8),
+        # boolean
+        (-np.inf, 1, np.bool_),
+        (0, np.inf, np.bool_),
     ],
 )
 def test_invalid_low_high(low, high, dtype):
-    with pytest.raises(
-        ValueError, match="Box low or high value are invalid for the dtype"
-    ):
-        Box(low=low, high=high, dtype=dtype)
+    if dtype == np.uint8 or dtype == np.bool_:
+        with pytest.raises(
+            ValueError, match=re.escape("Box unsigned int dtype don't support")
+        ):
+            Box(low=low, high=high, dtype=dtype)
+    else:
+        with pytest.raises(
+            ValueError, match=re.escape("value can be equal to `np.nan`,")
+        ):
+            Box(low=low, high=high, dtype=dtype)
 
 
 @pytest.mark.parametrize(
     "low, high, dtype",
     [
         # floats
+        (0, 1, float),
         (0, 1, np.float64),
         (0, 1, np.float32),
         (0, 1, np.float16),
@@ -184,6 +238,7 @@ def test_invalid_low_high(low, high, dtype):
         (-np.inf, np.inf, np.float32),
         (np.full((2,), -np.inf), np.full((2,), np.inf), np.float32),
         # signed ints
+        (0, 1, int),
         (0, 1, np.int64),
         (0, 1, np.int32),
         (0, 1, np.int16),
@@ -199,17 +254,25 @@ def test_invalid_low_high(low, high, dtype):
         (0, 1, np.uint32),
         (0, 1, np.uint16),
         (0, 1, np.uint8),
+        # boolean
+        (0, 1, np.bool_),
     ],
 )
 def test_valid_low_high(low, high, dtype):
     with warnings.catch_warnings(record=True) as caught_warnings:
         space = Box(low=low, high=high, dtype=dtype)
-        space.seed(0)
+        assert space.dtype == dtype
+        assert space.low.dtype == dtype
+        assert space.high.dtype == dtype
 
+        space.seed(0)
         sample = space.sample()
+        assert sample.dtype == dtype
         assert space.contains(sample)
 
-    assert len(caught_warnings) == 0, [x.message.args[0] for x in caught_warnings]
+    for warn in caught_warnings:
+        if "precision lowered by casting to float32" not in warn.message.args[0]:
+            raise Exception(warn)
 
 
 def test_contains_dtype():
