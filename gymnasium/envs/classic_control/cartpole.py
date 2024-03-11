22c8fd3adfe5a1a49fb33cde22ee99b22c613ff5
Kallinteris Andreas <30759571+Kallinteris-Andreas@users.noreply.github.com>
2024-03-11 20:49:23 +0200

    Update cartpole.py

--- gymnasium/envs/classic_control/cartpole.py
+++ gymnasium/envs/classic_control/cartpole.py
@@ -109,7 +109,7 @@ class CartPoleEnv(gym.Env[np.ndarray, Union[int, np.ndarray]]):
     ## Version History
     * v2: Changed reward function to the original from Barto, Sutton paper (related [GitHub Issue](https://github.com/Farama-Foundation/Gymnasium/issues/790)).
     * v1: `max_time_steps` raised to 500.
-        - In Gymnasium `1.0.0a2` the `sutton_barto_reward` was added (related [GitHub issue]()) 
+        - In Gymnasium `1.0.0a2` the `sutton_barto_reward` was added (related [GitHub issue](https://github.com/Farama-Foundation/Gymnasium/issues/790)) 
     * v0: Initial versions release.
     """
 
