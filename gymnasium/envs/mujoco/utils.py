d074b8693a47792395e34d03b18604b4b76128b8
Kallinteris Andreas <30759571+Kallinteris-Andreas@users.noreply.github.com>
2024-03-13 15:49:59 +0200

    `check_mujoco_reset_state` add state_type argument

--- gymnasium/envs/mujoco/utils.py
+++ gymnasium/envs/mujoco/utils.py
@@ -49,16 +49,27 @@ def set_state(
     return state
 
 
-def check_mujoco_reset_state(env: gymnasium.envs.mujoco.MujocoEnv, seed=1234):
-    """Asserts that `env.reset` properly resets the state (not affected by previous steps), assuming `check_reset_seed` has passed."""
+def check_mujoco_reset_state(
+        env: gymnasium.envs.mujoco.MujocoEnv,
+        seed=1234
+        state_type: mujoco.mjtState = mujoco.mjtState.mjSTATE_INTEGRATION,
+    ):
+    """Asserts that `env.reset()` properly resets the state (not affected by previous steps).
+    
+    Note: assuming `check_reset_seed` has passed.
+
+    Arguments:
+        env: Environment which is being tested.
+        seed: the `seed` used in `env.reset(seed)`.
+        state_type: see the [documentation of mjtState](https://mujoco.readthedocs.io/en/stable/APIreference/APItypes.html#mjtstate)."""
     env.action_space.seed(seed)
     action = env.action_space.sample()
 
     env.reset(seed=seed)
-    first_reset_state = get_state(env, mujoco.mjtState.mjSTATE_INTEGRATION)
+    first_reset_state = get_state(env, state_type)
     env.step(action)
 
     env.reset(seed=seed)
-    second_reset_state = get_state(env, mujoco.mjtState.mjSTATE_INTEGRATION)
+    second_reset_state = get_state(env, state_type)
 
     assert np.all(first_reset_state == second_reset_state), "reset is not deterministic"
