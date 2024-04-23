92c4f279034cc99867c1d52efa157655052dbf20
Mark Towers <mark.m.towers@gmail.com>
2024-04-23 23:08:52 +0100

    Add `wrappers.vector.HumanRendering` (#1013)

--- tests/wrappers/test_human_rendering.py
+++ tests/wrappers/test_human_rendering.py
@@ -26,7 +26,7 @@ def test_human_rendering():
     with pytest.raises(
         AssertionError,
         match=re.escape(
-            "Expected env.render_mode to be one of 'rgb_array' or 'rgb_array_list' but got 'human'"
+            "Expected env.render_mode to be one of ['rgb_array', 'rgb_array_list', 'depth_array', 'depth_array_list'] but got 'human'"
         ),
     ):
         HumanRendering(env)
