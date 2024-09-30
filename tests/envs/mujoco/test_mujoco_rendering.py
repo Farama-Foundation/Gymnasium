6a7e8a96385cc716c45c572cda985bddc0e8e10b
Kallinteris Andreas <kallinteris@protonmail.com>
2024-09-30 13:03:37 +0300

    extend `camera_id` test, by testing `render()`

--- tests/envs/mujoco/test_mujoco_rendering.py
+++ tests/envs/mujoco/test_mujoco_rendering.py
@@ -93,3 +93,7 @@ def test_camera_id(render_mode: str):
 
     assert env_a.mujoco_renderer.camera_id == env_b.mujoco_renderer.camera_id
     assert env_a.mujoco_renderer.camera_id != env_c.mujoco_renderer.camera_id
+
+    if render_mode != "human":
+        assert env_a.render() == env_b.render()
+        assert env_a.render() != env_c.render()
