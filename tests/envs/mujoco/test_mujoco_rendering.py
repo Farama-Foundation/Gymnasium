c6001d41b5ae0b9dde2cb0262d6259a9e1eb8bf3
Kallinteris Andreas <kallinteris@protonmail.com>
2024-09-30 13:54:13 +0300

    typo

--- tests/envs/mujoco/test_mujoco_rendering.py
+++ tests/envs/mujoco/test_mujoco_rendering.py
@@ -95,5 +95,5 @@ def test_camera_id(render_mode: str):
     assert env_a.mujoco_renderer.camera_id != env_c.mujoco_renderer.camera_id
 
     if render_mode != "human":
-        assert env_a.render() == env_b.render()
-        assert env_a.render() != env_c.render()
+        assert (env_a.render() == env_b.render()).all()
+        assert (env_a.render() != env_c.render()).all()
