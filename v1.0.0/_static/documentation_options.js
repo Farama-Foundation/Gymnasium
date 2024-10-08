96710b120f0f5072f70dd79eca16ddc799c8d5bd
pseudo-rnd-thoughts <mark.m.towers@gmail.com>
2024-10-08 10:03:43 +0000

    Deploying to gh-pages from @ Farama-Foundation/Gymnasium@196625488fc3fafef74cd97ace58ef625a41ad8c 🚀

---
+++ v1.0.0/_static/documentation_options.js
@@ -0,0 +1,13 @@
+const DOCUMENTATION_OPTIONS = {
+    VERSION: '1.0.0',
+    LANGUAGE: 'en',
+    COLLAPSE_INDEX: false,
+    BUILDER: 'dirhtml',
+    FILE_SUFFIX: '.html',
+    LINK_SUFFIX: '.html',
+    HAS_SOURCE: false,
+    SOURCELINK_SUFFIX: '.txt',
+    NAVIGATION_WITH_KEYS: false,
+    SHOW_SEARCH_SUMMARY: true,
+    ENABLE_SEARCH_SHORTCUTS: true,
+};
