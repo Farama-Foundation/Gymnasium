407ddd1b5cab89840fbc4f31796746221190fe6b
sparisi <sparisi@users.noreply.github.com>
2024-11-07 01:43:58 -0700

    sticky actions for more than 1 steps
    Optional argument that allows to repeat the old action for more than 1 step.

--- gymnasium/wrappers/stateful_action.py
+++ gymnasium/wrappers/stateful_action.py
@@ -18,7 +18,8 @@ class StickyAction(
     """Adds a probability that the action is repeated for the same ``step`` function.
 
     This wrapper follows the implementation proposed by `Machado et al., 2018 <https://arxiv.org/pdf/1709.06009.pdf>`_
-    in Section 5.2 on page 12.
+    in Section 5.2 on page 12, and adds the possibility to repeat the action for
+    more than one step.
 
     No vector version of the wrapper exists.
 
@@ -42,17 +43,25 @@ class StickyAction(
     """
 
     def __init__(
-        self, env: gym.Env[ObsType, ActType], repeat_action_probability: float
+        self, env: gym.Env[ObsType, ActType],
+        repeat_action_probability: float,
+        repeat_action_duration: int = 1,
     ):
         """Initialize StickyAction wrapper.
 
         Args:
-            env (Env): the wrapped environment
-            repeat_action_probability (int | float): a probability of repeating the old action.
+            env (Env): the wrapped environment,
+            repeat_action_probability (int | float): a probability of repeating the old action,
+            repeat_action_duration (int): the number of steps the action is repeated.
         """
         if not 0 <= repeat_action_probability < 1:
             raise InvalidProbability(
                 f"repeat_action_probability should be in the interval [0,1). Received {repeat_action_probability}"
+            )
+
+        if repeat_action_duration < 1:
+            raise ValueError(
+                f"repeat_action_duration should be larger or equal than 1. Received {repeat_action_duration}"
             )
 
         gym.utils.RecordConstructorArgs.__init__(
@@ -61,23 +70,35 @@ class StickyAction(
         gym.ActionWrapper.__init__(self, env)
 
         self.repeat_action_probability = repeat_action_probability
+        self.repeat_action_duration = repeat_action_duration
         self.last_action: ActType | None = None
+        self.last_action_repeats = 0
+        self.is_repeating = False
 
     def reset(
         self, *, seed: int | None = None, options: dict[str, Any] | None = None
     ) -> tuple[ObsType, dict[str, Any]]:
         """Reset the environment."""
         self.last_action = None
+        self.last_action_repeats = 0
+        self.is_repeating = False
 
         return super().reset(seed=seed, options=options)
 
     def action(self, action: ActType) -> ActType:
         """Execute the action."""
         if (
-            self.last_action is not None
+            self.is_repeating
+            or self.last_action is not None
             and self.np_random.uniform() < self.repeat_action_probability
         ):
             action = self.last_action
+            self.is_repeating = True
+            self.last_action_repeats += 1
+
+        if self.last_action_repeats == self.repeat_action_duration:
+            self.is_repeating = False
+            self.last_action_repeats = 0
 
         self.last_action = action
         return action
