aad84e31c4511bbc2701999d786431b590cbbc35
Adam Parker <foreverska@protonmail.com>
2025-03-19 18:28:12 -0500

    Restore old logic as dry transitions, seperate out wet transitions logic

--- gymnasium/envs/toy_text/taxi.py
+++ gymnasium/envs/toy_text/taxi.py
@@ -165,28 +165,57 @@ class TaxiEnv(Env):
         "render_fps": 4,
     }
 
-    def __init__(
-        self,
-        render_mode: Optional[str] = None,
-        is_rainy: bool = False,
-        fickle_passenger: bool = False,
+    def __build_dry_transitions(
+        self, num_rows, num_columns, max_row, max_col, locs, num_actions
     ):
-        self.desc = np.asarray(MAP, dtype="c")
+        for row in range(num_rows):
+            for col in range(num_columns):
+                for pass_idx in range(len(locs) + 1):  # +1 for being inside taxi
+                    for dest_idx in range(len(locs)):
+                        state = self.encode(row, col, pass_idx, dest_idx)
+                        if pass_idx < 4 and pass_idx != dest_idx:
+                            self.initial_state_distrib[state] += 1
+                        for action in range(num_actions):
+                            # defaults
+                            new_row, new_col, new_pass_idx = row, col, pass_idx
+                            reward = (
+                                -1
+                            )  # default reward when there is no pickup/dropoff
+                            terminated = False
+                            taxi_loc = (row, col)
 
-        self.locs = locs = [(0, 0), (0, 4), (4, 0), (4, 3)]
-        self.locs_colors = [(255, 0, 0), (0, 255, 0), (255, 255, 0), (0, 0, 255)]
+                            if action == 0:
+                                new_row = min(row + 1, max_row)
+                            elif action == 1:
+                                new_row = max(row - 1, 0)
+                            if action == 2 and self.desc[1 + row, 2 * col + 2] == b":":
+                                new_col = min(col + 1, max_col)
+                            elif action == 3 and self.desc[1 + row, 2 * col] == b":":
+                                new_col = max(col - 1, 0)
+                            elif action == 4:  # pickup
+                                if pass_idx < 4 and taxi_loc == locs[pass_idx]:
+                                    new_pass_idx = 4
+                                else:  # passenger not at location
+                                    reward = -10
+                            elif action == 5:  # dropoff
+                                if (taxi_loc == locs[dest_idx]) and pass_idx == 4:
+                                    new_pass_idx = dest_idx
+                                    terminated = True
+                                    reward = 20
+                                elif (taxi_loc in locs) and pass_idx == 4:
+                                    new_pass_idx = locs.index(taxi_loc)
+                                else:  # dropoff at wrong location
+                                    reward = -10
+                            new_state = self.encode(
+                                new_row, new_col, new_pass_idx, dest_idx
+                            )
+                            self.P[state][action].append(
+                                (1.0, new_state, reward, terminated)
+                            )
 
-        num_states = 500
-        num_rows = 5
-        num_columns = 5
-        max_row = num_rows - 1
-        max_col = num_columns - 1
-        self.initial_state_distrib = np.zeros(num_states)
-        num_actions = 6
-        self.P = {
-            state: {action: [] for action in range(num_actions)}
-            for state in range(num_states)
-        }
+    def __build_rainy_transitions(
+        self, num_rows, num_columns, max_row, max_col, locs, num_actions
+    ):
         for row in range(num_rows):
             for col in range(num_columns):
                 for pass_idx in range(len(locs) + 1):  # +1 for being inside taxi
@@ -266,7 +295,7 @@ class TaxiEnv(Env):
                             intended_state = self.encode(
                                 new_row, new_col, new_pass_idx, dest_idx
                             )
-                            if action <= 3 and is_rainy:
+                            if action <= 3:
                                 left_state = self.encode(
                                     left_pos[0], left_pos[1], new_pass_idx, dest_idx
                                 )
@@ -286,6 +315,38 @@ class TaxiEnv(Env):
                                 self.P[state][action].append(
                                     (1.0, intended_state, reward, terminated)
                                 )
+
+    def __init__(
+        self,
+        render_mode: Optional[str] = None,
+        is_rainy: bool = False,
+        fickle_passenger: bool = False,
+    ):
+        self.desc = np.asarray(MAP, dtype="c")
+
+        self.locs = locs = [(0, 0), (0, 4), (4, 0), (4, 3)]
+        self.locs_colors = [(255, 0, 0), (0, 255, 0), (255, 255, 0), (0, 0, 255)]
+
+        num_states = 500
+        num_rows = 5
+        num_columns = 5
+        max_row = num_rows - 1
+        max_col = num_columns - 1
+        self.initial_state_distrib = np.zeros(num_states)
+        num_actions = 6
+        self.P = {
+            state: {action: [] for action in range(num_actions)}
+            for state in range(num_states)
+        }
+
+        if is_rainy:
+            self.__build_rainy_transitions(
+                num_rows, num_columns, max_row, max_col, locs, num_actions
+            )
+        else:
+            self.__build_dry_transitions(
+                num_rows, num_columns, max_row, max_col, locs, num_actions
+            )
         self.initial_state_distrib /= self.initial_state_distrib.sum()
         self.action_space = spaces.Discrete(num_actions)
         self.observation_space = spaces.Discrete(num_states)
