a4ca9dda3be96c44096c55584c652440d81a835f
Tayzn <tyguy2464@gmail.com>
2025-04-25 16:39:17 -0400

    Formatting corrections

--- gymnasium/envs/toy_text/frozen_lake.py
+++ gymnasium/envs/toy_text/frozen_lake.py
@@ -156,11 +156,11 @@ class FrozenLakeEnv(Env):
     ```python
     import gymnasium as gym
     gym.make(
-        'FrozenLake-v1', 
-        desc=None, 
+        'FrozenLake-v1',
+        desc=None,
         map_name="4x4",
-        is_slippery=True, 
-        success_rate=1.0/3.0, 
+        is_slippery=True,
+        success_rate=1.0/3.0,
         reward_schedule=(1, 0, 0)
     )
     ```
